b28373cf31cd6ab6cb805390c4693c044a63328d
theoliverlear <sigwartholiver2@gmail.com>
2024-12-03 19:42:06 -0600

    Add date_utilities.py to provide date time functionalities throughout the backend application

---
+++ backend/apps/models/date_utilities.py
@@ -0,0 +1,16 @@
+import logging
+from datetime import datetime
+
+
+def get_next_friday():
+    today = datetime.now()
+    while today.weekday() != 4:
+        today = today.replace(day=today.day + 1)
+    return today
+
+def get_second_friday():
+    day = get_next_friday()
+    return get_next_week(day)
+
+def get_next_week(day):
+    return day.replace(day=day.day + 7)
