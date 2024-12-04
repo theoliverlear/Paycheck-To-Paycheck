b53637ba3f5add9c5da38e1ba7eade4cb3a09d5d
theoliverlear <sigwartholiver2@gmail.com>
2024-12-03 19:44:41 -0600

    Add interest_debt.py to represent a debt with interest model in the backend

---
+++ backend/apps/entity/holding/debt/interest_debt.py
@@ -0,0 +1,9 @@
+from attr import attr
+from attrs import define
+
+from backend.apps.entity.holding.interest_holding import InterestHolding
+
+
+@define
+class InterestDebt(InterestHolding):
+    interest_rate: float = attr(default=0.00)
