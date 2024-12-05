081f79116e139a359eebe5e95e13343d30fbb382
theoliverlear <sigwartholiver2@gmail.com>
2024-12-05 13:16:33 -0600

    Add component imports and export array for UI elements in elements.ts

---
+++ angular/components/elements/elements.ts
@@ -0,0 +1,15 @@
+import {SsAnchorComponent} from "./ss-anchor/ss-anchor.component";
+import {SsButtonComponent} from "./ss-button/ss-button.component";
+import {SsFooterComponent} from "./ss-footer/ss-footer.component";
+import {SsHeadComponent} from "./ss-head/ss-head.component";
+import {SsImgComponent} from "./ss-img/ss-img.component";
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+
+export const elements = [
+    NavBarComponent,
+    SsAnchorComponent,
+    SsButtonComponent,
+    SsFooterComponent,
+    SsHeadComponent,
+    SsImgComponent,
+]
