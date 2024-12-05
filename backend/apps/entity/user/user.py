f2fc9198887d450f75b99c65f804a5c5272a32a5
theoliverlear <sigwartholiver2@gmail.com>
2024-12-05 13:36:34 -0600

    Refactor user income management from Income to RecurringIncome in user.py to more accurately create a user profile

--- backend/apps/entity/user/user.py
+++ backend/apps/entity/user/user.py
@@ -1,7 +1,7 @@
 from attr import attr
 from attrs import define
 
-from backend.apps.entity.income.income import Income
+from backend.apps.entity.income.recurring_income import RecurringIncome
 from backend.apps.entity.user.safe_password import SafePassword
 
 
@@ -10,5 +10,5 @@ class User:
     first_name: str = attr(default="")
     last_name: str = attr(default="")
     username: str = attr(default="")
-    income: Income = attr(default=Income())
-    password: SafePassword = attr(default=SafePassword())
+    recurring_income: RecurringIncome = attr(factory=RecurringIncome)
+    password: SafePassword = attr(factory=SafePassword)
