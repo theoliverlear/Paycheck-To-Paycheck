4878f9e18e0b00ebaa8d5278eeeaa73a8cf73298
theoliverlear <sigwartholiver2@gmail.com>
2024-12-05 13:25:28 -0600

    Add core dependencies and services to paycheck-to-paycheck-app.module.ts

--- angular/modules/paycheck-to-paycheck-app.module.ts
+++ angular/modules/paycheck-to-paycheck-app.module.ts
@@ -1,10 +1,40 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from "../components/app/app.component";
+import {
+    HttpClientModule,
+    provideHttpClient,
+    withFetch
+} from "@angular/common/http";
+import {services} from "../services/services";
+import {NgOptimizedImage} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./routes/app-routing.module";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import {pages} from "../components/pages/pages";
+import {directives} from "../directives/directives";
+import {elements} from "../components/elements/elements";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [],
-    providers: [],
+    declarations: [
+        AppComponent,
+        ...elements,
+        ...directives,
+        ...pages,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        AppRoutingModule,
+        RouterOutlet,
+        NgOptimizedImage,
+        HttpClientModule],
+    providers: [
+        ...services,
+        provideHttpClient(withFetch()),
+    ],
     bootstrap: [AppComponent],
     exports: [],
     schemas: []
