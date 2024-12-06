8ee6dc068fbda0466e33a38f421c00bc8c1ed4a9
theoliverlear <sigwartholiver2@gmail.com>
2024-12-06 17:09:15 -0600

    Remove unnecessary newline in interest_debt.py to clean up code formatting

--- backend/apps/entity/holding/debt/interest_debt.py
+++ backend/apps/entity/holding/debt/interest_debt.py
@@ -3,7 +3,6 @@ from attrs import define
 
 from backend.apps.entity.holding.interest_holding import InterestHolding
 
-
 @define
 class InterestDebt(InterestHolding):
     interest_rate: float = attr(default=0.00)
