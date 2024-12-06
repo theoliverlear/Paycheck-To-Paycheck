714ac4e76422ec73a75c41962b1938032ad59b79
theoliverlear <sigwartholiver2@gmail.com>
2024-12-06 17:02:48 -0600

    Refactor IncomeType from an Enum to a class using attrs in income_type.py to allow for more flexible instantiation and default values

--- backend/apps/entity/income/income_type.py
+++ backend/apps/entity/income/income_type.py
@@ -1,11 +1,9 @@
-from enum import Enum
+from attr import attr
+from attrs import define
 
 from backend.apps.entity.time.date_interval import DateInterval
 
-class IncomeType(Enum):
-    SALARY = ('salary', DateInterval.YEARLY),
-    PAYCHECK = ('paycheck', DateInterval.BI_WEEKLY),
-    WAGE = ('wage', DateInterval.HOURLY),
-    ONE_TIME = ('one_time', DateInterval.NO_INTERVAL),
-    YEARLY_BONUS = ('yearly_bonus', DateInterval.YEARLY),
-    TAX_REFUND = ('tax_refund', DateInterval.YEARLY),
+@define
+class IncomeType:
+    name: str = attr(default="")
+    interval: DateInterval = attr(default=DateInterval.MONTHLY)
