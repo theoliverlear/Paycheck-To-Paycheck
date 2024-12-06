29b7bac32795010e810c49b48c4b76191978c0f3
theoliverlear <sigwartholiver2@gmail.com>
2024-12-06 16:47:00 -0600

    Add tax deduction functionality to paycheck_view.py

--- backend/apps/views/paycheck/paycheck_view.py
+++ backend/apps/views/paycheck/paycheck_view.py
@@ -1,12 +1,43 @@
+import logging
+
 from django.http import HttpResponse
 from rest_framework.views import APIView
 
+from backend.apps.entity.income.income import Income
+from backend.apps.entity.income.income_type import IncomeType
+from backend.apps.entity.income.income_types import IncomeTypes
+from backend.apps.entity.income.recurring_income import RecurringIncome
 from backend.apps.entity.paycheck.paycheck import Paycheck
+from backend.apps.entity.paycheck.taxed_paycheck import TaxedPaycheck
+from backend.apps.entity.tax.tax import Tax
+from backend.apps.entity.time.date_interval import DateInterval
+from backend.apps.entity.time.recurring_date import RecurringDate
+from backend.apps.entity.user.safe_password import SafePassword
 from backend.apps.entity.user.user import User
 
 
 class PaycheckView(APIView):
     def get(self, request, params, *args, **kwargs):
-        user = User(recurring_income=params)
-        paycheck = Paycheck(user.recurring_income)
-        return HttpResponse(paycheck)
+        user: User = User(
+            first_name='John',
+            last_name='Doe',
+            username='johndoe',
+            password=SafePassword('password'),
+            recurring_income=RecurringIncome(
+                income=params,
+                income_type=IncomeTypes.SALARY.value,
+                recurring_date=RecurringDate(1, DateInterval.YEARLY)
+            ),
+        )
+        tax: Tax = Tax(25)
+        taxed_paycheck: TaxedPaycheck = TaxedPaycheck(income=user.recurring_income, tax=tax)
+        logging.info(taxed_paycheck)
+        paycheck_str: str = f"${taxed_paycheck.paycheck_income:,.2f}"
+
+        paycheck_after_taxes_str: str = f"${taxed_paycheck.after_tax_income:,.2f}"
+        logging.info(f"Paycheck: {paycheck_str}, After Taxes: {paycheck_after_taxes_str}")
+        combined_info: str = f"Paycheck: {paycheck_str}, After Taxes: {paycheck_after_taxes_str}"
+        user.recurring_income.recurring_date.save()
+        user.recurring_income.save()
+        user.save()
+        return HttpResponse(combined_info)
