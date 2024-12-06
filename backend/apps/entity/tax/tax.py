c24893385e4ac0f5a7a859b48a69232e4e8b4953
theoliverlear <sigwartholiver2@gmail.com>
2024-12-06 16:57:08 -0600

    Add tax.py to define Tax class for calculating taxation in backend application

---
+++ backend/apps/entity/tax/tax.py
@@ -0,0 +1,17 @@
+from attr import attr
+from attrs import define
+
+
+@define
+class Tax:
+    rate: float = attr(default=0.0)
+
+    def amount_after_tax(self, amount: float):
+        return amount - self.amount_to_deduct(amount)
+
+    def amount_to_deduct(self, amount: float):
+        tax_amount = amount * self.rate_as_decimal()
+        return tax_amount
+
+    def rate_as_decimal(self):
+        return self.rate / 100
