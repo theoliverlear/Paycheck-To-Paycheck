e72a5661d8dc62a1eb55645ffa21314c84a015c7
theoliverlear <sigwartholiver2@gmail.com>
2024-12-06 16:52:37 -0600

    Refactor SafePassword class in safe_password.py to inherit from OrmCompatible and implement ORM saving functionality

--- backend/apps/entity/user/safe_password.py
+++ backend/apps/entity/user/safe_password.py
@@ -1,13 +1,17 @@
+from abc import ABC
+
 import bcrypt
-from attr import attr
-from attrs import define
+from django.db import models
+
+from backend.apps.entity.orm_compatible import OrmCompatible
+from backend.apps.entity.user.models import SafePasswordOrmModel
 
 
-@define(init=False)
-class SafePassword:
-    encoded_password: str = attr(default='')
-    ENCODING_TYPE: str = 'utf-8'
+class SafePassword(OrmCompatible, ABC):
+    encoded_password = models.CharField(max_length=100)
+    ENCODING_TYPE = 'utf-8'
     def __init__(self, unhashed_password: str=''):
+        super().__init__()
         self.encoded_password = self.hash_password(unhashed_password)
 
     def set_new_unhashed_password(self, unhashed_password: str):
@@ -15,8 +19,22 @@ class SafePassword:
 
     def hash_password(self, unhashed_password: str) -> str:
         salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(unhashed_password.encode(self.ENCODING_TYPE), salt)
-        return hashed_password.decode(self.ENCODING_TYPE)
+        hashed_password = bcrypt.hashpw(unhashed_password.encode(SafePassword.ENCODING_TYPE), salt)
+        return hashed_password.decode(SafePassword.ENCODING_TYPE)
 
     def compare_unencoded_password(self, unencoded_password: str) -> bool:
-        return bcrypt.checkpw(unencoded_password.encode(self.ENCODING_TYPE), self.encoded_password.encode(self.ENCODING_TYPE))
+        return bcrypt.checkpw(unencoded_password.encode(SafePassword.ENCODING_TYPE), self.encoded_password.encode(SafePassword.ENCODING_TYPE))
+
+    def save(self):
+        orm_model: SafePasswordOrmModel = self.get_orm_model()
+        orm_model.save()
+
+    def get_orm_model(self):
+        return SafePasswordOrmModel(
+            encoded_password=self.encoded_password
+        )
+
+    def set_from_orm_model(self, orm_model):
+        self.encoded_password = orm_model.encoded_password
+
+
