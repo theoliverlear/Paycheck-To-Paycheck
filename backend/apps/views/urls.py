4b35a2f42ae8ac61472861e40ba39e705ae91d54
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:24:10 -0600

    Add user path in urls.py to include user-related URLs

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -5,4 +5,5 @@ from backend.apps.views.home_view import HomeView
 urlpatterns = [
     path('<str:param>', HomeView.as_view(), name='home'),
     path('paycheck/', include('apps.views.paycheck.urls')),
+    path('user/', include('apps.views.user.urls')),
 ]
