f26e15cee27f1942d9b4da1f2c56cf9083d3e3cf
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:31:28 -0600

    Add abstract methods update, set_orm_model, and from_orm_model to orm_compatible.py to extend ORM integration

--- backend/apps/entity/orm_compatible.py
+++ backend/apps/entity/orm_compatible.py
@@ -1,18 +1,34 @@
 from abc import ABC, abstractmethod
+from typing import TypeVar
 
 from attrs import define
 
+T = TypeVar("T")
 
 @define
 class OrmCompatible(ABC):
     @abstractmethod
-    def save(self):
+    def save(self) -> T:
         pass
 
     @abstractmethod
-    def set_from_orm_model(self, orm_model):
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_from_orm_model(self, orm_model) -> None:
         pass
 
     @abstractmethod
     def get_orm_model(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def set_orm_model(db_model, model_to_set) -> None:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def from_orm_model(orm_model) -> T:
         pass
