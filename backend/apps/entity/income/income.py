b13605327bb3c29dbb0a7e395497f71a2781a833
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:48:07 -0600

    Add Identifiable inheritance to Income class and refactor save method implementation in income.py

--- backend/apps/entity/income/income.py
+++ backend/apps/entity/income/income.py
@@ -3,6 +3,7 @@ from abc import ABC
 from attr import attr
 from attrs import define
 
+from backend.apps.entity.identifiable import Identifiable
 from backend.apps.entity.income.income_type import IncomeType
 from backend.apps.entity.income.income_types import IncomeTypes
 from backend.apps.entity.income.models import IncomeOrmModel
@@ -10,13 +11,29 @@ from backend.apps.entity.orm_compatible import OrmCompatible
 
 
 @define
-class Income(OrmCompatible, ABC):
+class Income(OrmCompatible, ABC, Identifiable):
     income: float = attr(default=0.0)
     income_type: IncomeType = attr(default=IncomeTypes.SALARY)
 
     def save(self):
+        saved_income_type = self.income_type.save()
         orm_model: IncomeOrmModel = self.get_orm_model()
-        orm_model.save()
+        saved_income = IncomeOrmModel.objects.create(
+            id=orm_model.id,
+            income=orm_model.income,
+            income_type=saved_income_type
+        )
+        return Income.from_orm_model(saved_income)
+
+    def update(self):
+        pass
+
+    @staticmethod
+    def from_orm_model(orm_model):
+        income = Income()
+        income.income = orm_model.income
+        income.income_type = IncomeType.from_orm_model(orm_model.income_type)
+        return income
 
     def set_from_orm_model(self, orm_model):
         self.income = orm_model.income
