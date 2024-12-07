fc75045624eba96cd1637c1f1f00ce6164ad58fc
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:45:30 -0600

    Add primary key fields to TaxOrmModel and TaxWithholdingOrmModel in models.py

--- backend/apps/entity/tax/models.py
+++ backend/apps/entity/tax/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class TaxOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     rate = models.FloatField(default=0.0)
     class Meta:
         db_table = 'taxes'
 
 class TaxWithholdingOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     total_withheld = models.FloatField(default=0.0)
     class Meta:
         db_table = 'tax_withholdings'
