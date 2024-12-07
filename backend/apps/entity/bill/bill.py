590fb7cd45340d16cf65639923f67b236780c1a6
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:42:15 -0600

    Add Identifiable inheritance to Bill class in bill.py to ensure consistent entity identification

--- backend/apps/entity/bill/bill.py
+++ backend/apps/entity/bill/bill.py
@@ -1,10 +1,11 @@
 from attr import attr
 from attrs import define
 
+from backend.apps.entity.identifiable import Identifiable
 from backend.apps.entity.time.due_date import DueDate
 
 @define
-class Bill:
+class Bill(Identifiable):
     name: str = attr(default="")
     amount: float = attr(default=0.0)
     due_date: DueDate = attr(factory=DueDate)
