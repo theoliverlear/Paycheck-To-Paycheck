7fd4f389c6ddc1654f2e26a10038278f006529b9
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:50:38 -0600

    Refactor IncomeOrmModel and RecurringIncomeOrmModel in models.py to use ForeignKey for income_type and add primary key id fields for improved database relationships

--- backend/apps/entity/income/models.py
+++ backend/apps/entity/income/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 
-from backend.apps.entity.income.income_types import IncomeTypes
 from backend.apps.entity.time.date_interval import DateInterval
 from backend.apps.entity.time.recurring_date import RecurringDateOrmModel
 
 
-class IncomeOrmModel(models.Model):
-    income = models.FloatField(default=0.0)
-    income_type = models.CharField(max_length=100, default=IncomeTypes.SALARY)
-    class Meta:
-        db_table = 'incomes'
-
 class IncomeTypeOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     interval = models.IntegerField(default=DateInterval.YEARLY.value)
     class Meta:
         db_table = 'income_types'
 
 class RecurringIncomeOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     income = models.FloatField(default=0.0)
-    income_type = models.CharField(max_length=100, default=IncomeTypes.SALARY)
+    income_type = models.ForeignKey(IncomeTypeOrmModel, on_delete=models.CASCADE)
     recurring_date = models.ForeignKey(RecurringDateOrmModel, on_delete=models.CASCADE)
     class Meta:
         db_table = 'recurring_incomes'
+
+class IncomeOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    income = models.FloatField(default=0.0)
+    income_type = models.ForeignKey(IncomeTypeOrmModel, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'incomes'
