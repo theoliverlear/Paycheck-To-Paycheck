912bb8ffb1dd1301d0588ee045cc4350c27ce83d
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:21:40 -0600

    Add dependency injection configuration for UserRepository and UserService in app_module.py

--- backend/apps/injector/app_module.py
+++ backend/apps/injector/app_module.py
@@ -1,3 +1,8 @@
+from injector import Binder, singleton
+
+from backend.apps.repository.user_repository import UserRepository
+from backend.apps.services.user_service import UserService
+
 class AppModule:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -5,3 +10,7 @@ class AppModule:
     def __call__(self, request):
         response = self.get_response(request)
         return response
+
+    def configure(self, binder: Binder):
+        binder.bind(UserRepository, to=UserRepository, scope=singleton)
+        binder.bind(UserService, to=UserService, scope=singleton)
