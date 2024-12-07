c2e9c0be4b25850b2d5453f1ac4981196c83f91e
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:54:05 -0600

    Add primary key id field to HoldingOrmModel and InterestHoldingOrmModel in models.py

--- backend/apps/entity/holding/models.py
+++ backend/apps/entity/holding/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class HoldingOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     amount = models.FloatField(default=0.0)
 
 
 class InterestHoldingOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     amount = models.FloatField(default=0.0)
     interest_rate = models.FloatField(default=0.0)
