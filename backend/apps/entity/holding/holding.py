ed0ec2ad9cf5e0a67e1e2fdb6d9be16bffee3cd5
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:40:35 -0600

    Add Identifiable inheritance to Holding class in holding.py to ensure consistent entity identification

--- backend/apps/entity/holding/holding.py
+++ backend/apps/entity/holding/holding.py
@@ -3,6 +3,8 @@ from abc import ABC
 from attr import attr
 from attrs import define
 
+from backend.apps.entity.identifiable import Identifiable
+
 @define
-class Holding(ABC):
+class Holding(ABC, Identifiable):
     amount: float = attr(default=0.0)
