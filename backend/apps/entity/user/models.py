c87e694ba2ab8100359c197bfc56cdd101e7e8e7
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:33:09 -0600

    Add AutoField primary key id to SafePasswordOrmModel in user/models.py for unique identification

--- backend/apps/entity/user/models.py
+++ backend/apps/entity/user/models.py
@@ -4,6 +4,7 @@ from backend.apps.entity.income.models import RecurringIncomeOrmModel
 
 
 class SafePasswordOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     encoded_password = models.CharField(max_length=100)
     class Meta:
         db_table = 'passwords'
