684b5fed8a692e78e2e323cb380c93e8157eda4b
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:53:08 -0600

    Add primary key id field to InterestSavingOrmModel and SavingOrmModel in models.py

--- backend/apps/entity/holding/saving/models.py
+++ backend/apps/entity/holding/saving/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class InterestSavingOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     amount = models.FloatField(default=0.0)
     interest_rate = models.FloatField(default=0.0)
     class Meta:
         db_table = 'interest_savings'
 
 class SavingOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
     amount = models.FloatField(default=0.0)
     class Meta:
         db_table = 'savings'
