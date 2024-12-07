96df2790bc32430621d9841ea326fb0b19d72aa8
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:43:00 -0600

    Add Identifiable inheritance to RecurringBill class in recurring_bill.py to ensure consistent entity identification

--- backend/apps/entity/bill/recurring_bill.py
+++ backend/apps/entity/bill/recurring_bill.py
@@ -1,10 +1,11 @@
 from attr import attr
 from attrs import define
 
+from backend.apps.entity.identifiable import Identifiable
 from backend.apps.entity.time.recurring_date import RecurringDate
 
 @define
-class RecurringBill:
+class RecurringBill(Identifiable):
     name: str = attr(default="")
     amount: float = attr(default=0.0)
     recurring_date: RecurringDate = attr(factory=RecurringDate)
