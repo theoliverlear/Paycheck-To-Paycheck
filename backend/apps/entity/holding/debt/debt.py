6f009f800aad0dbce7bd7984c47f1311a34fc247
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 17:41:29 -0600

    Add Identifiable inheritance to Debt class in debt.py to ensure consistent entity identification

--- backend/apps/entity/holding/debt/debt.py
+++ backend/apps/entity/holding/debt/debt.py
@@ -2,8 +2,9 @@ from attr import attr
 from attrs import define
 
 from backend.apps.entity.holding.holding import Holding
+from backend.apps.entity.identifiable import Identifiable
 
 
 @define
-class Debt(Holding):
+class Debt(Holding, Identifiable):
     amount: float = attr(default=0.0)
