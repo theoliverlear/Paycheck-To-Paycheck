a1b92b6a49845744d707cbbc741efc28229988b7
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 20:55:12 -0600

    Add user_request.py to define a user http request model in the Django application

---
+++ backend/apps/comm/request/user_request.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class UserRequest(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
