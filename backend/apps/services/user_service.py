e0225e75e7f10914faeaa41b4b4f2a26d3dea4d9
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 21:24:37 -0600

    Add method get_by_username for retrieving user by username in user_service.py

--- backend/apps/services/user_service.py
+++ backend/apps/services/user_service.py
@@ -7,3 +7,6 @@ class UserService:
     @inject
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
+
+    def get_by_username(self, username: str):
+        return self.user_repository.get_by_username(username)
