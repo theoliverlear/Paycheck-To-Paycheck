1f2ac0150c6de77191d91a3fa9254104e95be0ab
theoliverlear <sigwartholiver2@gmail.com>
2024-12-09 22:56:42 -0600

    Add signup functionality using AuthService in signup_view.py

--- backend/apps/views/authorize/signup_view.py
+++ backend/apps/views/authorize/signup_view.py
@@ -1,6 +1,24 @@
+from injector import inject
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from backend.apps.comm.serialize.comm.request.signup_request_serializer import \
+    SignupRequestSerializer
+from backend.apps.services.auth_service import AuthService
 
 
 class SignupView(APIView):
-    def post(self, request, *args, **kwargs):
-        pass
+    @inject
+    def __init__(self,
+                 auth_service: AuthService):
+        super().__init__()
+        self.auth_service = auth_service
+    def post(self, http_request, *args, **kwargs):
+        serializer: SignupRequestSerializer = SignupRequestSerializer(data=http_request.data)
+        if serializer.is_valid():
+            signup_request = serializer.get_instance()
+            payload_status = self.auth_service.signup(signup_request, http_request)
+            return Response(payload_status.payload,
+                            status=payload_status.status_code)
+        else:
+            return Response(serializer.errors, status=400)
