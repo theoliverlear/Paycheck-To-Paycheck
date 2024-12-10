bf2f0f3a63acc3a69d8b65aa49e46281af76662e
theoliverlear <sigwartholiver2@gmail.com>
2024-12-09 22:54:51 -0600

    Refactor login endpoint in login_view.py to use LoginRequest and AuthStatusResponse and implement dependency injection for AuthService

--- backend/apps/views/authorize/login_view.py
+++ backend/apps/views/authorize/login_view.py
@@ -1,28 +1,30 @@
-from kombu.asynchronous.http import Response
+from injector import inject
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from backend.apps.comm.request.user_request import UserRequest
-from backend.apps.comm.response.auth_status import AuthStatus
-from backend.apps.comm.serialize.comm.request.user_request_serializer import \
-    UserRequestSerializer
+from backend.apps.comm.request.login_request import LoginRequest
+from backend.apps.comm.response.auth_status_response import AuthStatusResponse
+from backend.apps.comm.serialize.comm.request.login_request_serializer import \
+    LoginRequestSerializer
 from backend.apps.models.http.payload_status_response import \
     PayloadStatusResponse
 from backend.apps.services.auth_service import AuthService
 
 
 class LoginView(APIView):
+    @inject
     def __init__(self,
                  auth_service: AuthService):
         super().__init__()
         self.auth_service = auth_service
 
-    def post(self, request, *args, **kwargs):
-        serializer: UserRequestSerializer = UserRequestSerializer(data=request.data)
+    def post(self, http_request, *args, **kwargs):
+        serializer: LoginRequestSerializer = LoginRequestSerializer(data=http_request.data)
         if serializer.is_valid():
-            user_request: UserRequest = serializer.get_instance()
-            payload_status: PayloadStatusResponse[AuthStatus] = self.auth_service.login(user_request)
-            # TODO: Save user to session
-            return Response(payload_status.payload, status=payload_status.status)
+            login_request: LoginRequest = serializer.get_instance()
+            payload_status: PayloadStatusResponse[AuthStatusResponse] = self.auth_service.login(login_request, http_request)
+            return Response(payload_status.payload,
+                            status=payload_status.status_code)
         else:
             return Response(serializer.errors, status=400)
 
