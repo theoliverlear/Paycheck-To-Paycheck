7b38c93c7583ce0f75d1394fcb55a97b66cd236b
theoliverlear <sigwartholiver2@gmail.com>
2024-12-09 22:51:46 -0600

    Add user_from_request method to create User object from SignupRequest or LoginRequest in user_service.py

--- backend/apps/services/user_service.py
+++ backend/apps/services/user_service.py
@@ -1,5 +1,9 @@
 from injector import inject
 
+from backend.apps.comm.request.login_request import LoginRequest
+from backend.apps.comm.request.signup_request import SignupRequest
+from backend.apps.entity.user.safe_password import SafePassword
+from backend.apps.entity.user.user import User
 from backend.apps.repository.user_repository import UserRepository
 
 
@@ -8,5 +12,14 @@ class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    def get_by_username(self, username: str):
+    def user_from_request(self, user_request: SignupRequest | LoginRequest) -> User:
+        user: User = User(
+            username=user_request.username,
+            password=SafePassword(user_request.password)
+        )
+        if isinstance(user_request, SignupRequest):
+            user.email = user_request.email
+        return user
+
+    def get_by_username(self, username: str) -> User:
         return self.user_repository.get_by_username(username)
