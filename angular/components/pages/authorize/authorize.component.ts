c3b20ad64547d1fd8bcbacf5e3f8224a7bce8d5e
theoliverlear <sigwartholiver2@gmail.com>
2025-01-04 08:30:56 -0600

    Add authorize.component.ts to be an Angular component for the authorize element in the Angular application

---
+++ angular/components/pages/authorize/authorize.component.ts
@@ -0,0 +1,13 @@
+// authorize.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'authorize',
+    templateUrl: './authorize.component.html',
+    styleUrls: ['./authorize-style.component.css']
+})
+export class AuthorizeComponent {
+    constructor() {
+        
+    }
+}
