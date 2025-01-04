6da8153e016671228eff9c6293754ee7417d1be0
theoliverlear <sigwartholiver2@gmail.com>
2025-01-04 08:32:41 -0600

    Add welcome.component.ts to be an Angular component for the welcome element in the Angular application

---
+++ angular/components/pages/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+// welcome.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'welcome',
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome-style.component.css']
+})
+export class WelcomeComponent {
+    constructor() {
+        
+    }
+}
