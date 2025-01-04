961fe0d88c14a21c2f693fffe4e082975d69477c
theoliverlear <sigwartholiver2@gmail.com>
2025-01-04 07:59:25 -0600

    Add new components to elements.ts to inject new functionality into the Angular application

--- angular/components/elements/elements.ts
+++ angular/components/elements/elements.ts
@@ -4,12 +4,20 @@ import {SsFooterComponent} from "./ss-footer/ss-footer.component";
 import {SsHeadComponent} from "./ss-head/ss-head.component";
 import {SsImgComponent} from "./ss-img/ss-img.component";
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {NavItemComponent} from "./nav-item/nav-item.component";
+import {NameInputComponent} from "./name-input/name-input.component";
+import {WorkInputComponent} from "./work-input/work-input.component";
+import {IncomeInputComponent} from "./income-input/income-input.component";
 
 export const elements = [
+    IncomeInputComponent,
+    NameInputComponent,
     NavBarComponent,
+    NavItemComponent,
     SsAnchorComponent,
     SsButtonComponent,
     SsFooterComponent,
     SsHeadComponent,
     SsImgComponent,
+    WorkInputComponent,
 ]
