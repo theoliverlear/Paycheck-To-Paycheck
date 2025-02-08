8e37f1a5fc349b44b188b78b81e4fed05f5a5668
theoliverlear <sigwartholiver2@gmail.com>
2025-02-07 18:03:22 -0600

    Add income-input.component.ts to be an Angular component for the income-input element in the Angular application

---
+++ angular/components/elements/income-input/income-input.component.ts
@@ -0,0 +1,13 @@
+// income-input.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'income-input',
+    templateUrl: './income-input.component.html',
+    styleUrls: ['./income-input-style.component.css']
+})
+export class IncomeInputComponent {
+    constructor() {
+        
+    }
+}
