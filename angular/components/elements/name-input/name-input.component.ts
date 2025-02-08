17ee9048b0ae11d9bd6bb825a9733f3a3f28854c
theoliverlear <sigwartholiver2@gmail.com>
2025-02-07 18:04:37 -0600

    Add name-input.component.ts to be an Angular component for the name-input element in the Angular application

---
+++ angular/components/elements/name-input/name-input.component.ts
@@ -0,0 +1,13 @@
+// name-input.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'name-input',
+    templateUrl: './name-input.component.html',
+    styleUrls: ['./name-input-style.component.css']
+})
+export class NameInputComponent {
+    constructor() {
+        
+    }
+}
