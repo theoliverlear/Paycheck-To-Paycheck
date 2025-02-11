883e24f497c4493bcab79b78ec2d57525e4e92fa
theoliverlear <sigwartholiver2@gmail.com>
2025-02-11 10:07:43 -0600

    Add AccountComponent to the pages array in pages.ts

--- angular/components/pages/pages.ts
+++ angular/components/pages/pages.ts
@@ -2,8 +2,10 @@ import {HomeComponent} from "./home/home.component";
 import {PaychecksComponent} from "./paychecks/paychecks.component";
 import {AuthorizeComponent} from "./authorize/authorize.component";
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {AccountComponent} from "./account/account.component";
 
 export const pages = [
+    AccountComponent,
     AuthorizeComponent,
     HomeComponent,
     PaychecksComponent,
