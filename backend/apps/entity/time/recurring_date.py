93fafbf8ed2386a4e97fa394195fdbdb1963403a
theoliverlear <sigwartholiver2@gmail.com>
2025-02-13 07:38:59 -0600

    Add methods to calculate next recurring date and replace DateInterval with YearInterval in recurring_date.py

--- backend/apps/entity/time/recurring_date.py
+++ backend/apps/entity/time/recurring_date.py
@@ -1,20 +1,38 @@
 from abc import ABC
+from datetime import date, timedelta
 
 from attr import attr
 from attrs import define
 
 from backend.apps.entity.identifiable import Identifiable
 from backend.apps.entity.orm_compatible import OrmCompatible
-from backend.apps.entity.time.date_interval import DateInterval
+from backend.apps.entity.time.year_interval import YearInterval
 from backend.apps.entity.time.models import RecurringDateOrmModel
 from backend.apps.exception.entity_not_found_exception import \
     EntityNotFoundException
+from backend.apps.models.date_utilities import get_tomorrow, get_next_week, \
+    get_next_bi_week, get_next_month, get_next_year
 
 
 @define
-class RecurringDate(OrmCompatible['RecurringDate'], ABC, Identifiable):
-    day: int = attr(default=1)
-    interval: DateInterval = attr(default=DateInterval.MONTHLY)
+class RecurringDate(OrmCompatible['RecurringDate', RecurringDateOrmModel], ABC, Identifiable):
+    day: date = attr(default=date.today())
+    interval: YearInterval = attr(default=YearInterval.MONTHLY)
+
+    def get_next_date(self) -> date:
+        match self.interval:
+            case YearInterval.DAILY:
+                return get_tomorrow(self.day)
+            case YearInterval.WEEKLY:
+                return get_next_week(self.day)
+            case YearInterval.BI_WEEKLY:
+                return get_next_bi_week(self.day)
+            case YearInterval.MONTHLY:
+                return get_next_month(self.day)
+            case YearInterval.YEARLY:
+                return get_next_year(self.day)
+
+    # ORM
     def save(self) -> 'RecurringDate':
         orm_model: RecurringDateOrmModel = self.get_orm_model()
         saved_recurring_date_orm: RecurringDateOrmModel = RecurringDateOrmModel.objects.create(
@@ -41,7 +59,7 @@ class RecurringDate(OrmCompatible['RecurringDate'], ABC, Identifiable):
     def set_from_orm_model(self, orm_model) -> None:
         self.id = orm_model.id
         self.day = orm_model.day
-        self.interval = DateInterval.from_interval(orm_model.interval)
+        self.interval = YearInterval.from_interval(orm_model.interval)
 
     def get_orm_model(self) -> RecurringDateOrmModel:
         return RecurringDateOrmModel(
