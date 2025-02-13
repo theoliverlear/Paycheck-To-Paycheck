4d375524ab6dfc46a91e2a9da44af7b0f558b756
theoliverlear <sigwartholiver2@gmail.com>
2025-02-13 07:35:20 -0600

    Refactor interval constants to represent intermixing levels of date time in date_interval.py

--- backend/apps/entity/time/date_interval.py
+++ backend/apps/entity/time/date_interval.py
@@ -2,18 +2,8 @@ from enum import Enum
 
 
 class DateInterval(Enum):
-    YEARLY: int = 1
-    QUARTERLY: int = 4
-    BI_MONTHLY: int = 6
-    MONTHLY: int = 12
-    BI_WEEKLY: int = 26
-    WEEKLY: int = 52
-    DAILY: int = 365
-    HOURLY: int = 8760
-    NO_INTERVAL: int = 0
-    @staticmethod
-    def from_interval(interval: int) -> 'DateInterval':
-        for date_interval in DateInterval:
-            if date_interval.value == interval:
-                return date_interval
-        return DateInterval.NO_INTERVAL
+    DAYS_IN_WEEK: int = 7
+    DAYS_IN_BI_WEEK: int = 14
+    DAYS_IN_YEAR: int = 365
+    WEEKS_IN_MONTH: int = 4
+    WEEKS_IN_YEAR: int = 52
