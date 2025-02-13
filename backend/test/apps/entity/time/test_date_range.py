5f30210a7ad602ff3c545969736b3387e9af45d9
theoliverlear <sigwartholiver2@gmail.com>
2025-02-13 07:46:35 -0600

    Add tests for verifying failing date ranges in test_date_range.py

--- backend/test/apps/entity/time/test_date_range.py
+++ backend/test/apps/entity/time/test_date_range.py
@@ -1,7 +1,8 @@
 import unittest
 from datetime import date
 
-from backend.apps.models.date_utilities import get_next_bi_week, get_next_week
+from backend.apps.models.date_utilities import get_next_bi_week, \
+    get_next_week, get_next_month
 from backend.apps.entity.time.date_range import DateRange
 
 
@@ -11,8 +12,17 @@ class MyTestCase(unittest.TestCase):
         ending_date=get_next_bi_week(date.today())
     )
     def test_in_range(self):
-        start_next_week: date = get_next_week(date.today())
-        self.assertTrue(self.date_range.in_range(start_next_week))
+        today: date = date.today()
+        start_next_week: date = get_next_week(today)
+        in_range: bool = self.date_range.in_range(start_next_week)
+        self.date_range.print_in_range(start_next_week)
+        self.assertTrue(in_range)
+
+        next_month: date = get_next_month(today)
+        in_range = self.date_range.in_range(next_month)
+        self.date_range.print_in_range(next_month)
+        self.assertFalse(in_range)
+
 
 
 if __name__ == '__main__':
