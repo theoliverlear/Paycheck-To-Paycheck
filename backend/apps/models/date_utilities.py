1c3e57cb067a5e00c05d80303af106f07e28a776
theoliverlear <sigwartholiver2@gmail.com>
2025-02-13 07:33:15 -0600

    Add utility functions for date calculations in date_utilities.py including `get_tomorrow`, `get_next_month`, `get_next_bi_month`, and `get_next_year`

--- backend/apps/models/date_utilities.py
+++ backend/apps/models/date_utilities.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 def get_next_friday():
@@ -11,8 +11,20 @@ def get_second_friday():
     day = get_next_friday()
     return get_next_week(day)
 
-def get_next_week(day):
-    return day.replace(day=day.day + 7)
+def get_tomorrow(day: date):
+    return day.replace(day=day.day + 1)
 
-def get_next_bi_week(day):
-    return day.replace(day=day.day + 14)
+def get_next_week(starting_date: date):
+    return starting_date.replace(day=starting_date.day + 7)
+
+def get_next_bi_week(starting_date: date):
+    return starting_date.replace(day=starting_date.day + 14)
+
+def get_next_month(starting_date: date):
+    return starting_date.replace(month=starting_date.month + 1)
+
+def get_next_bi_month(starting_date: date):
+    return starting_date.replace(month=starting_date.month + 2)
+
+def get_next_year(starting_date: date):
+    return starting_date.replace(year=starting_date.year + 1)
