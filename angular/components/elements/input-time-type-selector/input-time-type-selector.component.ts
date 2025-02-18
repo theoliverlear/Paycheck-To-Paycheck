79493d630c115b5dfb01a839cf5dec38bef1bf4a
theoliverlear <sigwartholiver2@gmail.com>
2025-02-18 16:59:13 -0600

    Add input-time-type-selector.component.ts to be an Angular component for the input-time-type-selector element in the Angular application

---
+++ angular/components/elements/input-time-type-selector/input-time-type-selector.component.ts
@@ -0,0 +1,13 @@
+// input-time-type-selector.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'input-time-type-selector',
+    templateUrl: './input-time-type-selector.component.html',
+    styleUrls: ['./input-time-type-selector.component.css']
+})
+export class InputTimeTypeSelectorComponent {
+    constructor() {
+        
+    }
+}
