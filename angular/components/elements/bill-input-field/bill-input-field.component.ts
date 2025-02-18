66d874ee5622cf29ecc6678211a1e22f021dcb67
theoliverlear <sigwartholiver2@gmail.com>
2025-02-18 16:53:48 -0600

    Add bill-input-field.component.ts to be an Angular component for the bill-input-field element in the Angular application

---
+++ angular/components/elements/bill-input-field/bill-input-field.component.ts
@@ -0,0 +1,13 @@
+// bill-input-field.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'bill-input-field',
+    templateUrl: './bill-input-field.component.html',
+    styleUrls: ['./bill-input-field.component.css']
+})
+export class BillInputFieldComponent {
+    constructor() {
+        
+    }
+}
