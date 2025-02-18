855cd36c82ded155103ec8955a1e5070493a23f3
theoliverlear <sigwartholiver2@gmail.com>
2025-02-18 16:12:39 -0600

    Change InputType from const enum to enum and add `RANGE` type in InputType.ts

--- angular/components/elements/ss-input/models/InputType.ts
+++ angular/components/elements/ss-input/models/InputType.ts
@@ -1,8 +1,9 @@
-export const enum InputType {
+export enum InputType {
     NUMBER = 'number',
     TEXT = 'text',
     PASSWORD = 'password',
     EMAIL = 'email',
     TELEPHONE_PAD = 'tel',
     CHECKBOX = 'checkbox',
+    RANGE = 'range'
 }
