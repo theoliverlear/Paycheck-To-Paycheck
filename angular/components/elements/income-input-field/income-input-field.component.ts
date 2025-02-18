8a08a42ef041a88d224fcce314930173c591bb5a
theoliverlear <sigwartholiver2@gmail.com>
2025-02-18 16:56:02 -0600

    Add income-input-field.component.ts to be an Angular component for the income-input-field element in the Angular application

---
+++ angular/components/elements/income-input-field/income-input-field.component.ts
@@ -0,0 +1,15 @@
+// income-input-field.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'income-input-field',
+    templateUrl: './income-input-field.component.html',
+    styleUrls: ['./income-input-field.component.css']
+})
+export class IncomeInputFieldComponent {
+    // TODO: If the income type of WAGE is selected, then the program should
+    //       display hours worked in addition to income.
+    constructor() {
+        
+    }
+}
