095b204f3b3f8ada6ac2bc8913358f20955f181d
theoliverlear <sigwartholiver2@gmail.com>
2025-02-19 12:31:34 -0600

    Add input initialization logic in bill-input-field.component.ts

--- angular/components/elements/bill-input-field/bill-input-field.component.ts
+++ angular/components/elements/bill-input-field/bill-input-field.component.ts
@@ -1,5 +1,8 @@
 // bill-input-field.component.ts 
-import { Component } from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+import {BillInputFieldType} from "./models/BillInputFieldType";
+import {InputType} from "../ss-input/models/InputType";
 
 @Component({
     selector: 'bill-input-field',
@@ -7,7 +10,27 @@ import { Component } from "@angular/core";
     styleUrls: ['./bill-input-field.component.css']
 })
 export class BillInputFieldComponent {
+    @Input() protected fieldType: BillInputFieldType;
     constructor() {
         
     }
+
+    public getInputTitle(): string {
+        return this.fieldType + ":";
+    }
+
+    public getInputType(): InputType {
+        switch (this.fieldType) {
+            case BillInputFieldType.TITLE:
+                return InputType.TEXT;
+            case BillInputFieldType.AMOUNT:
+                return InputType.NUMBER;
+            case BillInputFieldType.DATE:
+                return InputType.DATE;
+            default:
+                return InputType.TEXT;
+        }
+    }
+
+    protected readonly TagType = TagType;
 }
