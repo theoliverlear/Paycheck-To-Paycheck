249f71254e4f24edfeed5c6a00f93b658b802d41
theoliverlear <sigwartholiver2@gmail.com>
2025-02-19 12:22:12 -0600

    Add variables and methods for selecting and emitting time type in input-time-type-selector.component.ts

--- angular/components/elements/input-time-type-selector/input-time-type-selector.component.ts
+++ angular/components/elements/input-time-type-selector/input-time-type-selector.component.ts
@@ -1,5 +1,9 @@
 // input-time-type-selector.component.ts 
-import { Component } from "@angular/core";
+import {Component, EventEmitter, Output, ViewChild} from "@angular/core";
+import {InputTimeType} from "../../../models/input/InputTimeType";
+import {
+    InputTimeTypeComponent
+} from "../input-time-type/input-time-type.component";
 
 @Component({
     selector: 'input-time-type-selector',
@@ -7,7 +11,31 @@ import { Component } from "@angular/core";
     styleUrls: ['./input-time-type-selector.component.css']
 })
 export class InputTimeTypeSelectorComponent {
+    @ViewChild('oneTimeButton') oneTimeButton: InputTimeTypeComponent;
+    @ViewChild('recurringButton') recurringButton: InputTimeTypeComponent;
+    @Output() timeTypeSelected: EventEmitter<InputTimeType> = new EventEmitter<InputTimeType>();
     constructor() {
         
     }
+
+    selectOneTime(): void {
+        this.oneTimeButton.select();
+        this.recurringButton.deselect();
+        this.emitTimeType();
+    }
+
+    selectRecurring(): void {
+        this.oneTimeButton.deselect();
+        this.recurringButton.select();
+        this.emitTimeType();
+    }
+
+    public emitTimeType(): void {
+        if (this.oneTimeButton.selected) {
+            this.timeTypeSelected.emit(InputTimeType.ONE_TIME);
+        } else {
+            this.timeTypeSelected.emit(InputTimeType.RECURRING);
+        }
+    }
+    protected readonly InputTimeType = InputTimeType;
 }
