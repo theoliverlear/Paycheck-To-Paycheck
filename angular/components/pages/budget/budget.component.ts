25372b503806df50a7644ff1f6af038c278df571
theoliverlear <sigwartholiver2@gmail.com>
2025-02-19 20:56:37 -0600

    Add fade animation to BudgetComponent in budget.component.ts for animation effects

--- angular/components/pages/budget/budget.component.ts
+++ angular/components/pages/budget/budget.component.ts
@@ -1,10 +1,16 @@
 // budget.component.ts 
 import { Component } from "@angular/core";
+import {
+    fadeInOutAnimation
+} from "../../animations/animations";
 
 @Component({
     selector: 'budget',
     templateUrl: './budget.component.html',
-    styleUrls: ['./budget.component.css']
+    styleUrls: ['./budget.component.css'],
+    animations: [
+        fadeInOutAnimation
+    ]
 })
 export class BudgetComponent {
     constructor() {
