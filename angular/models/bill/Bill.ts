21e0fcd1bbe49b81ee5d51acbd81ab43589aae28
theoliverlear <sigwartholiver2@gmail.com>
2025-02-19 20:54:45 -0600

    Add properties and constructor to Bill.ts for handling bill details

--- angular/models/bill/Bill.ts
+++ angular/models/bill/Bill.ts
@@ -1,3 +1,17 @@
+import {InputTimeType} from "../input/InputTimeType";
+
 export class Bill {
-    // Title, amount, date, recurring or not
+    public title: string;
+    public amount: number;
+    public date: Date;
+    public timeType: InputTimeType;
+    public constructor(title: string = '',
+                       amount: number = 0,
+                       date: Date = new Date(),
+                       timeType: InputTimeType = InputTimeType.ONE_TIME) {
+        this.title = title;
+        this.amount = amount;
+        this.date = date;
+        this.timeType = timeType;
+    }
 }
