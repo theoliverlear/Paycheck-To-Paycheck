025c2c831eb1ff2c73b5d030436e516d859c80a0
theoliverlear <sigwartholiver2@gmail.com>
2025-02-19 21:12:39 -0600

    Add input event emitter and initialization logic to BillInputFieldComponent in bill-input-field.component.ts

--- angular/components/elements/bill-input-field/bill-input-field.component.ts
+++ angular/components/elements/bill-input-field/bill-input-field.component.ts
@@ -1,22 +1,35 @@
 // bill-input-field.component.ts 
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 import {TagType} from "../../../models/html/TagType";
 import {BillInputFieldType} from "./models/BillInputFieldType";
 import {InputType} from "../ss-input/models/InputType";
+import {BillInputContent} from "./models/BillInputContent";
 
 @Component({
     selector: 'bill-input-field',
     templateUrl: './bill-input-field.component.html',
     styleUrls: ['./bill-input-field.component.css']
 })
-export class BillInputFieldComponent {
+export class BillInputFieldComponent implements OnInit {
     @Input() protected fieldType: BillInputFieldType;
+    @Input() protected placeholder: string | number;
+    @Output() inputEvent: EventEmitter<BillInputContent> = new EventEmitter<BillInputContent>();
+    protected inputContent: BillInputContent = new BillInputContent();
     constructor() {
         
     }
 
+    ngOnInit() {
+        this.inputContent.fieldType = this.fieldType;
+    }
+
     public getInputTitle(): string {
         return this.fieldType + ":";
+    }
+
+    public emitInput(value: string | number): void {
+        this.inputContent.value = value;
+        this.inputEvent.emit(this.inputContent);
     }
 
     public getInputType(): InputType {
