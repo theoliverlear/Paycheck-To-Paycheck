5390595c05a7ca84a75b292313b2f1c348e6deb8
theoliverlear <sigwartholiver2@gmail.com>
2025-02-21 18:46:35 -0600

    Edit import paths in urls.py to include 'backend' for correct module resolution

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -4,6 +4,6 @@ from backend.apps.views.home_view import HomeView
 
 urlpatterns = [
     path('<str:param>', HomeView.as_view(), name='home'),
-    path('paycheck/', include('apps.views.paycheck.urls')),
-    path('user/', include('apps.views.user.urls')),
+    path('paycheck/', include('backend.apps.views.paycheck.urls')),
+    path('user/', include('backend.apps.views.user.urls')),
 ]
