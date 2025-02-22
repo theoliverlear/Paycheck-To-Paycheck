2561d828c5f019cd3c6c239b6c707dd28c3f241a
theoliverlear <sigwartholiver2@gmail.com>
2025-02-21 18:14:16 -0600

    Add optional billInterval for recurring bill functionality in Bill.ts

--- angular/models/bill/Bill.ts
+++ angular/models/bill/Bill.ts
@@ -1,17 +1,23 @@
 import {InputTimeType} from "../input/InputTimeType";
+import {
+    RecurringBillTimeInterval
+} from "../../components/elements/recurring-bill-dropdown/models/RecurringBillTimeInterval";
 
 export class Bill {
     public title: string;
     public amount: number;
     public date: Date;
     public timeType: InputTimeType;
+    public billInterval?: RecurringBillTimeInterval;
     public constructor(title: string = '',
                        amount: number = 0,
                        date: Date = new Date(),
-                       timeType: InputTimeType = InputTimeType.ONE_TIME) {
+                       timeType: InputTimeType = InputTimeType.ONE_TIME,
+                       billInterval?: RecurringBillTimeInterval) {
         this.title = title;
         this.amount = amount;
         this.date = date;
         this.timeType = timeType;
+        this.billInterval = billInterval;
     }
 }
