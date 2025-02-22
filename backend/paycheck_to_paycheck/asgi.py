09e2873a6c97676dd329ba5bad36cf78f1f55da3
theoliverlear <sigwartholiver2@gmail.com>
2025-02-21 18:47:51 -0600

    Refactor ASGI configuration in asgi.py to include WebSocket routing

--- backend/paycheck_to_paycheck/asgi.py
+++ backend/paycheck_to_paycheck/asgi.py
@@ -1,7 +1,28 @@
 import os
+import sys
 
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paycheck_to_paycheck.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'backend.paycheck_to_paycheck.settings')
 
-application = get_asgi_application()
+print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
+print("PYTHONPATH Content:", sys.path)
+
+django_application = get_asgi_application()
+
+from backend.apps.routing.routing import websocket_url_patterns
+
+application = ProtocolTypeRouter({
+    "http": django_application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_url_patterns
+        )
+    ),
+})
