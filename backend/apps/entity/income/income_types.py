f401ca1e4f374cfe680857b912b19a8acb18769e
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 11:28:02 -0600

    Refactor IncomeType constants to match new class definition in income_types.py

--- backend/apps/entity/income/income_types.py
+++ backend/apps/entity/income/income_types.py
@@ -5,17 +5,10 @@ from backend.apps.entity.time.year_interval import YearInterval
 
 
 class IncomeTypes(Enum):
-    SALARY       = IncomeType(name='salary',
-                              interval=YearInterval.YEARLY)
-    PAYCHECK     = IncomeType(name='paycheck',
-                              interval=YearInterval.BI_WEEKLY)
-    WAGE         = IncomeType(name='wage',
-                              interval=YearInterval.HOURLY)
-    MONTHLY      = IncomeType(name='monthly',
-                              interval=YearInterval.MONTHLY)
-    ONE_TIME     = IncomeType(name='one time',
-                              interval=YearInterval.NO_INTERVAL)
-    YEARLY_BONUS = IncomeType(name='yearly bonus',
-                              interval=YearInterval.YEARLY)
-    TAX_REFUND   = IncomeType(name='tax refund',
-                              interval=YearInterval.YEARLY)
+    SALARY       = IncomeType(interval=YearInterval.YEARLY)
+    PAYCHECK     = IncomeType(interval=YearInterval.BI_WEEKLY)
+    WAGE         = IncomeType(interval=YearInterval.HOURLY)
+    MONTHLY      = IncomeType(interval=YearInterval.MONTHLY)
+    ONE_TIME     = IncomeType(interval=YearInterval.NO_INTERVAL)
+    YEARLY_BONUS = IncomeType(interval=YearInterval.YEARLY)
+    TAX_REFUND   = IncomeType(interval=YearInterval.YEARLY)
