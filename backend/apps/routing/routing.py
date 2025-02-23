7c3703ab8b4ca21ed635fa24c681f1bbc6b6e55f
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 11:34:17 -0600

    Add income WebSocket route to URL patterns in routing.py

--- backend/apps/routing/routing.py
+++ backend/apps/routing/routing.py
@@ -1,7 +1,12 @@
-from django.urls import re_path
+from typing import List
+
+from django.urls import re_path, URLPattern, URLResolver
 
 from backend.apps.routing.websocket.bill.bill_consumer import BillConsumer
+from backend.apps.routing.websocket.income.income_consumer import \
+    IncomeConsumer
 
 websocket_url_patterns = [
-    re_path('ws/bill', BillConsumer.as_asgi())
+    re_path('ws/bill', BillConsumer.as_asgi()),
+    re_path('ws/income', IncomeConsumer.as_asgi())
 ]
