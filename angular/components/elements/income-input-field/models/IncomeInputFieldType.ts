3b3d6a8b35f8130ad16869f7fd1480b1ada8b6fb
theoliverlear <sigwartholiver2@gmail.com>
2025-02-22 20:03:16 -0600

    Add IncomeInputFieldType enum in IncomeInputFieldType.ts for defining income input field types

---
+++ angular/components/elements/income-input-field/models/IncomeInputFieldType.ts
@@ -0,0 +1,8 @@
+export enum IncomeInputFieldType {
+    // TODO: Abstract into a more common enum given it's a match with
+    //       BillInputFieldType.ts.
+    TITLE = 'Title',
+    AMOUNT = 'Amount',
+    HOURS = 'Hours',
+    DATE = 'Date',
+}
