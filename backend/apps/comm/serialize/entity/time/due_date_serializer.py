650c511677ea9922b540e038ee67a9817cf7c1a1
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 19:42:55 -0600

    Add date format specification for due_date in DueDateSerializer in due_date_serializer.py

--- backend/apps/comm/serialize/entity/time/due_date_serializer.py
+++ backend/apps/comm/serialize/entity/time/due_date_serializer.py
@@ -5,7 +5,8 @@ from backend.apps.entity.time.due_date import DueDate
 
 class DueDateSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=False)
-    due_date = serializers.DateField()
+    due_date = serializers.DateField(format="%Y-%m-%d")
+
     class Meta:
         model = DueDate,
         fields = '__all__'
