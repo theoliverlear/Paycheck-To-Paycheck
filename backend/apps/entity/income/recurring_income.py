8af8960d0a138dfc13dbcd6e56442fc211857177
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 20:00:04 -0600

    Refactor RecurringIncome to inherit from UndatedIncome and update attribute handling in recurring_income.py

--- backend/apps/entity/income/recurring_income.py
+++ backend/apps/entity/income/recurring_income.py
@@ -3,9 +3,8 @@ from abc import ABC
 from attr import attr
 from attrs import define
 
-from backend.apps.entity.income.income import Income
-from backend.apps.entity.income.income_type import IncomeType
 from backend.apps.entity.income.models import RecurringIncomeOrmModel
+from backend.apps.entity.income.undated_income import UndatedIncome
 from backend.apps.entity.orm_compatible import OrmCompatible
 from backend.apps.entity.time.recurring_date import RecurringDate
 from backend.apps.exception.entity_not_found_exception import \
@@ -13,16 +12,15 @@ from backend.apps.exception.entity_not_found_exception import \
 
 
 @define
-class RecurringIncome(Income, OrmCompatible['RecurringIncome', RecurringIncomeOrmModel], ABC):
+class RecurringIncome(UndatedIncome, OrmCompatible['RecurringIncome', RecurringIncomeOrmModel], ABC):
     recurring_date: RecurringDate = attr(factory=RecurringDate)
 
     def save(self) -> 'RecurringIncome':
         saved_recurring_date: RecurringDate = self.recurring_date.save()
-        income_type: IncomeType = self.income_type.save()
         orm_model: RecurringIncomeOrmModel = self.get_orm_model()
         saved_recurring_income: RecurringIncomeOrmModel = RecurringIncomeOrmModel.objects.create(
-            income=orm_model.income,
-            income_type=income_type.get_orm_model(),
+            name=orm_model.name,
+            income_amount=orm_model.income_amount,
             recurring_date=saved_recurring_date.get_orm_model()
         )
         return RecurringIncome.from_orm_model(saved_recurring_income)
@@ -38,23 +36,25 @@ class RecurringIncome(Income, OrmCompatible['RecurringIncome', RecurringIncomeOr
             raise EntityNotFoundException(self)
 
     @staticmethod
-    def set_orm_model(db_model, model_to_match) -> RecurringIncomeOrmModel:
-        db_model.income = model_to_match.income
-        db_model.income_type = model_to_match.income_type
+    def set_orm_model(db_model: RecurringIncomeOrmModel,
+                      model_to_match: RecurringIncomeOrmModel) -> RecurringIncomeOrmModel:
+        db_model.id = model_to_match.id
+        db_model.name = model_to_match.name
+        db_model.income_amount = model_to_match.income_amount
         db_model.recurring_date = model_to_match.recurring_date
         return db_model
 
-    def set_from_orm_model(self, orm_model) -> None:
+    def set_from_orm_model(self, orm_model: RecurringIncomeOrmModel) -> None:
         self.id = orm_model.id
-        self.income = orm_model.income
-        self.income_type = IncomeType.from_orm_model(orm_model.income_type)
+        self.name = orm_model.name
+        self.income_amount = orm_model.income_amount
         self.recurring_date = RecurringDate.from_orm_model(orm_model.recurring_date)
 
     def get_orm_model(self) -> RecurringIncomeOrmModel:
         return RecurringIncomeOrmModel(
             id=self.id,
-            income=self.income,
-            income_type=self.income_type.get_orm_model(),
+            name=self.name,
+            income_amount=self.income_amount,
             recurring_date=self.recurring_date.get_orm_model()
         )
 
