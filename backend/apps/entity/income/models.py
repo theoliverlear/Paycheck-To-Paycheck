071b2bc06547f36ee185305b68b7e0e3d5b929a4
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 19:58:57 -0600

    Refactor income models in models.py to add OneTimeIncomeOrmModel and IncomeHistoryOrmModel

--- backend/apps/entity/income/models.py
+++ backend/apps/entity/income/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 from backend.apps.entity.time.year_interval import YearInterval
 from backend.apps.entity.time.recurring_date import RecurringDateOrmModel
@@ -12,14 +13,25 @@ class IncomeTypeOrmModel(models.Model):
 
 class RecurringIncomeOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
-    income = models.FloatField(default=0.0)
+    name = models.CharField(max_length=255, default='')
+    income_amount = models.FloatField(default=0.0)
     recurring_date = models.ForeignKey(RecurringDateOrmModel, on_delete=models.CASCADE)
     class Meta:
         db_table = 'recurring_incomes'
 
-class IncomeOrmModel(models.Model):
+class OneTimeIncomeOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, default='')
     income_amount = models.FloatField(default=0.0)
+    date_received = models.DateField(default=now)
     class Meta:
-        db_table = 'incomes'
+        db_table = 'one_time_incomes'
+
+class IncomeHistoryOrmModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField('user.UserOrmModel', on_delete=models.CASCADE,
+                                related_name='user_income_history',
+                                default=None)
+    class Meta:
+        db_table = 'income_histories'
+        abstract = False
