5853f53fe7283701c02e46b0a9deda3b9f6d597b
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 20:01:03 -0600

    Update PaycheckOrmModel and TaxedPaycheckOrmModel to use a one-to-one relationship with RecurringIncomeOrmModel in models.py

--- backend/apps/entity/paycheck/models.py
+++ backend/apps/entity/paycheck/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
-from backend.apps.entity.income.models import IncomeOrmModel
+from backend.apps.entity.income.models import RecurringIncomeOrmModel
 from backend.apps.entity.tax.models import TaxOrmModel
 
 
 class PaycheckOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
-    income = models.ForeignKey(IncomeOrmModel, on_delete=models.CASCADE)
+    income = models.OneToOneField(RecurringIncomeOrmModel, on_delete=models.CASCADE)
     paycheck_income = models.FloatField(default=0.0)
     class Meta:
         db_table = 'paychecks'
 
 class TaxedPaycheckOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
-    income = models.ForeignKey(IncomeOrmModel, on_delete=models.CASCADE)
+    income = models.OneToOneField(RecurringIncomeOrmModel, on_delete=models.CASCADE)
     paycheck_income = models.FloatField(default=0.0)
     tax = models.ForeignKey(TaxOrmModel, on_delete=models.CASCADE)
     after_tax_income = models.FloatField(default=0.0)
