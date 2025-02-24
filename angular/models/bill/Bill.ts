c503689ca59b4ac892b5579e06fdf7aaac91727c
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 19:36:37 -0600

    Rename 'title' property to 'name' in Bill.ts for consistency with naming conventions

--- angular/models/bill/Bill.ts
+++ angular/models/bill/Bill.ts
@@ -4,17 +4,17 @@ import {
 } from "../../components/elements/recurring-bill-dropdown/models/RecurringBillTimeInterval";
 
 export class Bill {
-    public title: string;
+    public name: string;
     public amount: number;
     public date: Date;
     public timeType: InputTimeType;
     public billInterval?: RecurringBillTimeInterval;
-    public constructor(title: string = '',
+    public constructor(name: string = '',
                        amount: number = 0,
                        date: Date = new Date(),
                        timeType: InputTimeType = InputTimeType.ONE_TIME,
                        billInterval?: RecurringBillTimeInterval) {
-        this.title = title;
+        this.name = name;
         this.amount = amount;
         this.date = date;
         this.timeType = timeType;
