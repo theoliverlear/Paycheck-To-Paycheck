f7f026360710da6724f47948b7b6538dea6428f6
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 20:26:52 -0600

    Inject dependencies into BillConsumer using Injector in routing.py

--- backend/apps/routing/routing.py
+++ backend/apps/routing/routing.py
@@ -1,12 +1,16 @@
 from typing import List
 
 from django.urls import re_path, URLPattern, URLResolver
+from injector import Injector
 
+from backend.apps.injector import AppModule
 from backend.apps.routing.websocket.bill.bill_consumer import BillConsumer
 from backend.apps.routing.websocket.income.income_consumer import \
     IncomeConsumer
 
+injector = Injector(AppModule())
+
 websocket_url_patterns = [
-    re_path('ws/bill', BillConsumer.as_asgi()),
+    re_path('ws/bill', BillConsumer.factory(injector)),
     re_path('ws/income', IncomeConsumer.as_asgi())
 ]
