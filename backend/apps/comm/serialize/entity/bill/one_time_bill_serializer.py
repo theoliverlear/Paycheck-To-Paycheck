432e955b1be74e9747d5b2153b3eefcca4011406
theoliverlear <sigwartholiver2@gmail.com>
2025-02-23 19:37:29 -0600

    Rename and refactor BillSerializer to OneTimeBillSerializer in one_time_bill_serializer.py to handle one-time bill entities

--- backend/apps/comm/serialize/entity/bill/one_time_bill_serializer.py
+++ backend/apps/comm/serialize/entity/bill/one_time_bill_serializer.py
@@ -2,16 +2,16 @@ from rest_framework import serializers
 
 from backend.apps.comm.serialize.entity.time.due_date_serializer import \
     DueDateSerializer
-from backend.apps.entity.bill.bill import Bill
+from backend.apps.entity.bill.one_time_bill import OneTimeBill
 
 
-class BillSerializer(serializers.Serializer):
+class OneTimeBillSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=False)
     name = serializers.CharField(max_length=255)
     amount = serializers.FloatField()
     due_date = DueDateSerializer()
     class Meta:
-        model = Bill
+        model = OneTimeBill
         fields = '__all__'
 
     def get_instance(self):
