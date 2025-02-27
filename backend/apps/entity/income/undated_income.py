9aab6adaa33ab98e349538d38369abef750242ea
theoliverlear <sigwartholiver2@gmail.com>
2025-02-27 12:18:44 -0600

    Add getter and setter for income amount variable in undated_income.py to encapsulate the attribute

--- backend/apps/entity/income/undated_income.py
+++ backend/apps/entity/income/undated_income.py
@@ -6,4 +6,12 @@ from backend.apps.entity.identifiable import Identifiable
 @define
 class UndatedIncome(Identifiable):
     name: str = attr(default="")
-    income_amount: float = attr(default=0.0)
+    _income_amount: float = attr(default=0.0)
+
+    @property
+    def income_amount(self):
+        return self._income_amount
+
+    @income_amount.setter
+    def income_amount(self, income_amount):
+        self._income_amount = income_amount
