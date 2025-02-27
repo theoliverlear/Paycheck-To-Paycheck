17bc3c90936725c95e61a55be0d65f8a0d53995d
theoliverlear <sigwartholiver2@gmail.com>
2025-02-27 12:06:42 -0600

    Add initialization check in identifiable.py to check if an entity is persisted

--- backend/apps/entity/identifiable.py
+++ backend/apps/entity/identifiable.py
@@ -5,6 +5,7 @@ from attrs import define
 @define
 class Identifiable:
     _id: int = attr(default=0)
+
     @property
     def id(self):
         return self._id
@@ -12,3 +13,6 @@ class Identifiable:
     @id.setter
     def id(self, new_id):
         self._id = new_id
+
+    def is_initialized(self):
+        return self.id is not 0
