762b0898ba5e84508bbe3e94888f9d63c87f56e0
theoliverlear <sigwartholiver2@gmail.com>
2025-02-27 12:05:41 -0600

    Refactor bill_history.py to simplify initialization and prevent double adding to bill lists

--- backend/apps/entity/bill/bill_history.py
+++ backend/apps/entity/bill/bill_history.py
@@ -3,24 +3,23 @@ from abc import ABC
 from typing import TYPE_CHECKING
 
 from attr import attr
+from attrs import define
 
-from backend.apps.entity.bill.one_time_bill import OneTimeBill
 from backend.apps.entity.bill.recurring_bill import RecurringBill
-from backend.resolve_class import resolve_class
+from backend.apps.exception.entity_not_found_exception import \
+    EntityNotFoundException
 
 if TYPE_CHECKING:
-    from backend.apps.entity.user.user import User
+    from backend.apps.entity.bill.one_time_bill import OneTimeBill
 
 from backend.apps.entity.bill.models import BillHistoryOrmModel
 from backend.apps.entity.identifiable import Identifiable
 from backend.apps.entity.orm_compatible import OrmCompatible
 
-
+@define
 class BillHistory(OrmCompatible['BillHistory', BillHistoryOrmModel], ABC, Identifiable):
-    user: 'User' = attr(factory=lambda: resolve_class('backend.apps.entity.user.user.User',
-                                                      'User')())
-    one_time_bills: list[OneTimeBill] = attr(factory=list[OneTimeBill])
-    recurring_bills: list[RecurringBill] = attr(factory=list[RecurringBill])
+    one_time_bills: list[OneTimeBill] = attr(default=[])
+    recurring_bills: list[RecurringBill] = attr(default=[])
 
     def add_one_time_bill(self, bill: OneTimeBill) -> None:
         self.one_time_bills.append(bill)
@@ -29,21 +28,20 @@ class BillHistory(OrmCompatible['BillHistory', BillHistoryOrmModel], ABC, Identi
         self.recurring_bills.append(recurring_bill)
 
     def save(self) -> 'BillHistory':
-        saved_user: User = self.user.save()
         saved_one_time_bills: list[OneTimeBill] = self.save_all_one_time_bills()
         saved_recurring_bills: list[RecurringBill] = self.save_all_recurring_bills()
         orm_model: BillHistoryOrmModel = self.get_orm_model()
-        saved_bill_history = BillHistoryOrmModel.objects.create(
-            user=saved_user.get_orm_model(),
-            one_time_bills=saved_one_time_bills,
-            recurring_bills=saved_recurring_bills
-        )
-        return BillHistory.from_orm_model(saved_bill_history)
+        if self.is_initialized():
+            self.update()
+            return self
+        else:
+            saved_bill_history = BillHistoryOrmModel.objects.create()
+            self.set_from_orm_model(saved_bill_history)
+            return BillHistory.from_orm_model(saved_bill_history)
 
     def update(self) -> None:
         try:
             db_model = BillHistoryOrmModel.objects.get(id=self.id)
-            self.user.update()
             self.update_all_one_time_bills()
             self.update_all_recurring_bills()
             orm_model: BillHistoryOrmModel = self.get_orm_model()
@@ -63,13 +61,17 @@ class BillHistory(OrmCompatible['BillHistory', BillHistoryOrmModel], ABC, Identi
     def save_all_one_time_bills(self) -> list[OneTimeBill]:
         saved_bills = []
         for bill in self.one_time_bills:
-            saved_bills.append(bill.save())
+            saved_bill: OneTimeBill = bill.save()
+            saved_bills.append(saved_bill)
+        self.one_time_bills = saved_bills
         return saved_bills
 
     def save_all_recurring_bills(self) -> list[RecurringBill]:
         saved_bills = []
         for bill in self.recurring_bills:
-            saved_bills.append(bill.save())
+            saved_bill: RecurringBill = bill.save()
+            saved_bills.append(saved_bill)
+        self.recurring_bills = saved_bills
         return saved_bills
 
     def get_orm_model(self) -> BillHistoryOrmModel:
@@ -78,17 +80,13 @@ class BillHistory(OrmCompatible['BillHistory', BillHistoryOrmModel], ABC, Identi
         recurring_bill_orm_models = [bill.get_orm_model() for
                                      bill in self.recurring_bills]
         return BillHistoryOrmModel(
-            id=self.id,
-            user=self.user.get_orm_model(),
-            one_time_bills=one_time_bill_orm_models,
-            recurring_bills=recurring_bill_orm_models
+            id=self.id
         )
 
     def set_from_orm_model(self, orm_model) -> None:
         self.id = orm_model.id
-        self.user = User.from_orm_model(orm_model.user)
-        self.set_one_time_bills_from_orm_model(orm_model)
-        self.set_recurring_bills_from_orm_model(orm_model)
+        # self.set_one_time_bills_from_orm_model(orm_model)
+        # self.set_recurring_bills_from_orm_model(orm_model)
 
     def set_recurring_bills_from_orm_model(self, orm_model):
         self.recurring_bills = []
@@ -103,9 +101,9 @@ class BillHistory(OrmCompatible['BillHistory', BillHistoryOrmModel], ABC, Identi
 
     @staticmethod
     def set_orm_model(db_model, model_to_match) -> None:
-        db_model.user = model_to_match.user
-        db_model.one_time_bills = model_to_match.one_time_bills
-        db_model.recurring_bills = model_to_match.recurring_bills
+        db_model.id = model_to_match.id
+        # db_model.one_time_bills = model_to_match.one_time_bills
+        # db_model.recurring_bills = model_to_match.recurring_bills
 
     @staticmethod
     def from_orm_model(orm_model: BillHistoryOrmModel) -> 'BillHistory':
