5a5c86b4fc2f3d829ede33873f22a7ed23153596
theoliverlear <sigwartholiver2@gmail.com>
2025-02-28 14:28:13 -0600

    Add income history attribute and related ORM handling in recurring_income.py

--- backend/apps/entity/income/recurring_income.py
+++ backend/apps/entity/income/recurring_income.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
 from abc import ABC
+from typing import TYPE_CHECKING
 
 from attr import attr
 from attrs import define
-
+if TYPE_CHECKING:
+    from backend.apps.entity.income.income_history import IncomeHistory
 from backend.apps.entity.income.models import RecurringIncomeOrmModel
 from backend.apps.entity.income.undated_income import UndatedIncome
 from backend.apps.entity.orm_compatible import OrmCompatible
@@ -15,6 +18,7 @@ from backend.apps.exception.entity_not_found_exception import \
 class RecurringIncome(UndatedIncome, OrmCompatible['RecurringIncome', RecurringIncomeOrmModel], ABC):
     _recurring_date: RecurringDate = attr(factory=RecurringDate)
     yearly_income: float = attr(default=0.0)
+    income_history: IncomeHistory = attr(default=None)
 
     @property
     def recurring_date(self):
@@ -42,18 +46,19 @@ class RecurringIncome(UndatedIncome, OrmCompatible['RecurringIncome', RecurringI
     def calculate_yearly_income(self):
         self.yearly_income = self._income_amount * self._recurring_date.interval.value
 
-
     def save(self) -> 'RecurringIncome':
         if self.is_initialized():
             self.update()
             return self
         else:
             saved_recurring_date: RecurringDate = self._recurring_date.save()
+            saved_income_history: IncomeHistory = self.income_history.save()
             orm_model: RecurringIncomeOrmModel = self.get_orm_model()
             saved_recurring_income: RecurringIncomeOrmModel = RecurringIncomeOrmModel.objects.create(
                 name=orm_model.name,
                 income_amount=orm_model.income_amount,
-                recurring_date=saved_recurring_date.get_orm_model()
+                recurring_date=saved_recurring_date.get_orm_model(),
+                income_history=saved_income_history.get_orm_model()
             )
             return RecurringIncome.from_orm_model(saved_recurring_income)
 
@@ -62,8 +67,8 @@ class RecurringIncome(UndatedIncome, OrmCompatible['RecurringIncome', RecurringI
             db_model = RecurringIncomeOrmModel.objects.get(id=self.id)
             self._recurring_date.update()
             orm_model: RecurringIncomeOrmModel = self.get_orm_model()
-            db_model: RecurringIncomeOrmModel = self.set_orm_model(db_model, orm_model)
-            db_model.save()
+            current_model: RecurringIncomeOrmModel = RecurringIncome.set_orm_model(db_model, orm_model)
+            current_model.save()
         except RecurringIncomeOrmModel.DoesNotExist as exception:
             raise EntityNotFoundException(self)
 
@@ -74,6 +79,7 @@ class RecurringIncome(UndatedIncome, OrmCompatible['RecurringIncome', RecurringI
         db_model.name = model_to_match.name
         db_model.income_amount = model_to_match.income_amount
         db_model.recurring_date = model_to_match.recurring_date
+        db_model.income_history = model_to_match.income_history
         return db_model
 
     def set_from_orm_model(self, orm_model: RecurringIncomeOrmModel) -> None:
@@ -81,13 +87,15 @@ class RecurringIncome(UndatedIncome, OrmCompatible['RecurringIncome', RecurringI
         self.name = orm_model.name
         self.income_amount = orm_model.income_amount
         self.recurring_date = RecurringDate.from_orm_model(orm_model.recurring_date)
+        self.income_history = orm_model.income_history.get_orm_model()
 
     def get_orm_model(self) -> RecurringIncomeOrmModel:
         return RecurringIncomeOrmModel(
             id=self.id,
             name=self.name,
             income_amount=self.income_amount,
-            recurring_date=self.recurring_date.get_orm_model()
+            recurring_date=self.recurring_date.get_orm_model(),
+            income_history=self.income_history.get_orm_model()
         )
 
     @staticmethod
