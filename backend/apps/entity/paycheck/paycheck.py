21aa88a14634bcee267ed3ca392595d4eff6070f
theoliverlear <sigwartholiver2@gmail.com>
2025-02-28 21:45:22 -0600

    Add methods for managing incomes and bills with validation in paycheck.py

--- backend/apps/entity/paycheck/paycheck.py
+++ backend/apps/entity/paycheck/paycheck.py
@@ -25,6 +25,15 @@ class Paycheck(Identifiable):
     total_income: float = attr(default=0.0)
     total_bills: float = attr(default=0.0)
 
+    @property
+    def date_range(self):
+        return self._date_range
+
+    @date_range.setter
+    def date_range(self, date_range):
+        self._date_range = date_range
+        self.purge_outdated_items()
+
     def __attrs_post_init__(self):
         self.update_totals()
 
@@ -37,6 +46,55 @@ class Paycheck(Identifiable):
 
     def update_total_bills(self):
         self.total_bills = self.get_total_bills()
+
+
+    def get_num_incomes(self) -> int:
+        return len(self.one_time_incomes) + len(self.recurring_incomes) + len(self.wage_incomes)
+
+    def get_num_bills(self) -> int:
+        return len(self.one_time_bills) + len(self.recurring_bills)
+
+    def add_one_time_income(self, one_time_income: OneTimeIncome) -> None:
+        contains_income: bool = one_time_income in self.one_time_incomes
+        self._date_range.print_in_range(one_time_income.date_received)
+        in_range: bool = self._date_range.in_range(one_time_income.date_received)
+        logging.info(f'One time income --- In range: {in_range}, Contains income {contains_income}')
+        if not contains_income and in_range:
+            self.one_time_incomes.append(one_time_income)
+            self.update_total_income()
+            self.purge_outdated_incomes()
+
+    def add_recurring_income(self, recurring_income: RecurringIncome) -> None:
+        contains_income: bool = recurring_income in self.recurring_incomes
+        in_range: bool = self._date_range.in_range(recurring_income.recurring_date.day)
+        if not contains_income and in_range:
+            self.recurring_incomes.append(recurring_income)
+            self.update_total_income()
+            self.purge_outdated_incomes()
+
+    def add_wage_income(self, wage_income: WageIncome):
+        contains_income: bool = wage_income in self.wage_incomes
+        in_range: bool = self._date_range.in_range(wage_income.recurring_date.day)
+        if not contains_income and in_range:
+            self.wage_incomes.append(wage_income)
+            self.update_total_income()
+            self.purge_outdated_incomes()
+
+    def add_one_time_bill(self, one_time_bill: OneTimeBill) -> None:
+        contains_bill: bool = one_time_bill in self.one_time_bills
+        in_range: bool = self._date_range.in_range(one_time_bill.due_date.due_date)
+        if not contains_bill and in_range:
+            self.one_time_bills.append(one_time_bill)
+            self.update_total_bills()
+            self.purge_outdated_bills()
+
+    def add_recurring_bill(self, recurring_bill: RecurringBill) -> None:
+        contains_bill: bool = recurring_bill in self.recurring_bills
+        in_range: bool = self._date_range.in_range(recurring_bill.recurring_date.day)
+        if not contains_bill and in_range:
+            self.recurring_bills.append(recurring_bill)
+            self.update_total_bills()
+            self.purge_outdated_bills()
 
     def get_total_income(self):
         total_yearly_income: float = 0.0
@@ -81,35 +139,48 @@ class Paycheck(Identifiable):
         #       method.
         updated_one_time_incomes: list[OneTimeIncome] = []
         for one_time_income in self.one_time_incomes:
-            if self._date_range.in_range(one_time_income.date_received):
+            contains_income = one_time_income in updated_one_time_incomes
+            if self._date_range.in_range(one_time_income.date_received) and not contains_income:
                 updated_one_time_incomes.append(one_time_income)
         self.one_time_incomes = updated_one_time_incomes
 
         updated_recurring_incomes: list[RecurringIncome] = []
         for recurring_income in self.recurring_incomes:
-            if self._date_range.in_range(recurring_income.recurring_date.day):
+            contains_income = recurring_income in updated_recurring_incomes
+            if self._date_range.in_range(recurring_income.recurring_date.day) and not contains_income:
                 updated_recurring_incomes.append(recurring_income)
         self.recurring_incomes = updated_recurring_incomes
 
         updated_wage_incomes: list[WageIncome] = []
         for wage_income in self.wage_incomes:
-            if self._date_range.in_range(wage_income.recurring_date.day):
+            contains_income = wage_income in updated_wage_incomes
+            if self._date_range.in_range(wage_income.recurring_date.day) and not contains_income:
                 updated_wage_incomes.append(wage_income)
         self.wage_incomes = updated_wage_incomes
+        self.update_total_income()
 
     def purge_outdated_bills(self):
         updated_one_time_bills: list[OneTimeBill] = []
         for one_time_bill in self.one_time_bills:
-            if self._date_range.in_range(one_time_bill.due_date.due_date):
+            contains_bill = one_time_bill in updated_one_time_bills
+            if self._date_range.in_range(one_time_bill.due_date.due_date) and not contains_bill:
                 updated_one_time_bills.append(one_time_bill)
         self.one_time_bills = updated_one_time_bills
 
         updated_recurring_bills: list[RecurringBill] = []
         for recurring_bill in self.recurring_bills:
-            if self._date_range.in_range(recurring_bill.recurring_date.day):
+            contains_bill = recurring_bill in updated_recurring_bills
+            if self._date_range.in_range(recurring_bill.recurring_date.day) and not contains_bill:
                 updated_recurring_bills.append(recurring_bill)
         self.recurring_bills = updated_recurring_bills
-        
+        self.update_total_bills()
+
+
+    def remove_one_time_income(self, income_title: str):
+        for index, one_time_income in enumerate(self.one_time_incomes):
+            if one_time_income.name == income_title:
+                self.one_time_incomes.pop(index)
+
     @staticmethod
     def from_user(user: User):
         paycheck: Paycheck = Paycheck()
