3158016cedf52b843cae53b59926e0b66ce175d9
theoliverlear <sigwartholiver2@gmail.com>
2025-02-28 21:44:38 -0600

    Refactor date calculation logic in date_utilities.py to fix edge case bugs

--- backend/apps/models/date_utilities.py
+++ backend/apps/models/date_utilities.py
@@ -1,9 +1,10 @@
 import logging
+from calendar import monthrange
 from datetime import datetime, date, timedelta
 
 
 def get_next_friday():
-    today = datetime.now()
+    today = date.today()
     while today.weekday() != 4:
         today = today.replace(day=today.day + 1)
     return today
@@ -12,8 +13,12 @@ def get_second_friday():
     day = get_next_friday()
     return get_next_week(day)
 
-def get_tomorrow(day: date):
-    return day.replace(day=day.day + 1)
+def get_tomorrow(day: date) -> date:
+    # return day.replace(day=day.day + 1)
+
+    tomorrow = day + timedelta(days=1)
+    logging.info(f'Tomorrow type: {type(tomorrow)}')
+    return tomorrow
 
 def get_next_week(starting_date: date):
     next_week_date = starting_date + timedelta(days=7)
@@ -28,10 +33,28 @@ def get_next_bi_week(starting_date: date):
     return next_bi_week_date
 
 def get_next_month(starting_date: date):
-    return starting_date.replace(month=starting_date.month + 1)
+    # return starting_date.replace(month=starting_date.month + 1)
+    next_month: int = starting_date.month + 1
+    year: int = starting_date.year
+    if next_month > 12:
+        next_month = 1
+        year += 1
+    valid_day: int = min(get_days_in_month(next_month, year),
+                         starting_date.day)
+    return date(year, next_month, valid_day)
+
+def get_days_in_month(month: int, year: int) -> int:
+    return monthrange(year, month)[1]
 
 def get_next_bi_month(starting_date: date):
-    return starting_date.replace(month=starting_date.month + 2)
+    next_month: int = starting_date.month + 1
+    year: int = starting_date.year
+    if next_month > 12:
+        next_month = 2
+        year += 1
+    valid_day: int = min(get_days_in_month(next_month, year),
+                         starting_date.day)
+    return date(year, next_month, valid_day)
 
 def get_next_year(starting_date: date):
     return starting_date.replace(year=starting_date.year + 1)
