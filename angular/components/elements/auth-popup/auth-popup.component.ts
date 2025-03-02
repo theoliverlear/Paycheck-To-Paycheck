eb66a9dae0493e79ba0ffba458561dd7f1ef024f
theoliverlear <sigwartholiver2@gmail.com>
2025-03-01 19:42:43 -0600

    Add auth-popup.component.ts to be an Angular component for the auth-popup element in the Angular application

---
+++ angular/components/elements/auth-popup/auth-popup.component.ts
@@ -0,0 +1,13 @@
+// auth-popup.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'auth-popup',
+    templateUrl: './auth-popup.component.html',
+    styleUrls: ['./auth-popup.component.css']
+})
+export class AuthPopupComponent {
+    constructor() {
+        
+    }
+}
