a62a30356bfbc4f5dc026192cd5a614fc0c5ba91
theoliverlear <sigwartholiver2@gmail.com>
2025-03-01 19:44:05 -0600

    Add login-console.component.ts to be an Angular component for the login-console element in the Angular application

---
+++ angular/components/elements/login-console/login-console.component.ts
@@ -0,0 +1,13 @@
+// login-console.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'login-console',
+    templateUrl: './login-console.component.html',
+    styleUrls: ['./login-console.component.css']
+})
+export class LoginConsoleComponent {
+    constructor() {
+        
+    }
+}
