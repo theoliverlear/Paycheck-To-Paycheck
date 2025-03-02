8454ea687bd7a0ee09f8c0aa208abdceded45713
theoliverlear <sigwartholiver2@gmail.com>
2025-03-01 19:46:57 -0600

    Add service-terms-input.component.ts to be an Angular component for the service-terms-input element in the Angular application

---
+++ angular/components/elements/service-terms-input/service-terms-input.component.ts
@@ -0,0 +1,13 @@
+// service-terms-input.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'service-terms-input',
+    templateUrl: './service-terms-input.component.html',
+    styleUrls: ['./service-terms-input.component.css']
+})
+export class ServiceTermsInputComponent {
+    constructor() {
+        
+    }
+}
