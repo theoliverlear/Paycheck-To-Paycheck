0049e380b64d699cacbe3c16e1a55cbcd40bd76b
theoliverlear <sigwartholiver2@gmail.com>
2025-03-01 19:43:22 -0600

    Add auth-type-slider.component.ts to be an Angular component for the auth-type-slider element in the Angular application

---
+++ angular/components/elements/auth-type-slider/auth-type-slider.component.ts
@@ -0,0 +1,13 @@
+// auth-type-slider.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'auth-type-slider',
+    templateUrl: './auth-type-slider.component.html',
+    styleUrls: ['./auth-type-slider.component.css']
+})
+export class AuthTypeSliderComponent {
+    constructor() {
+        
+    }
+}
