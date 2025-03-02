3d3427b2f2826ff35d46e9657f1e0e9927f3d43b
theoliverlear <sigwartholiver2@gmail.com>
2025-03-01 19:46:07 -0600

    Add login-inputs.component.ts to be an Angular component for the login-inputs element in the Angular application

---
+++ angular/components/elements/login-inputs/login-inputs.component.ts
@@ -0,0 +1,13 @@
+// login-inputs.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'login-inputs',
+    templateUrl: './login-inputs.component.html',
+    styleUrls: ['./login-inputs.component.css']
+})
+export class LoginInputsComponent {
+    constructor() {
+        
+    }
+}
