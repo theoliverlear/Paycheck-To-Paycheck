859f619e358815ad89ae0af0cf74b7f9bd2d5f81
theoliverlear <sigwartholiver2@gmail.com>
2025-03-03 14:16:21 -0600

    Add credential input handling functionality in signup-input.component.ts

--- angular/components/elements/signup-input/signup-input.component.ts
+++ angular/components/elements/signup-input/signup-input.component.ts
@@ -1,8 +1,18 @@
 // signup-input.component.ts
-import {Component, Input} from "@angular/core";
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild
+} from "@angular/core";
 import {AuthInputType} from "../../../models/auth/AuthInputType";
 import {TagType} from "../../../models/html/TagType";
 import {InputType} from "../ss-input/models/InputType";
+import {
+    SignupCredentials
+} from "../../../models/auth/credentials/SignupCredentials";
+import {SsInputComponent} from "../ss-input/ss-input.component";
 
 @Component({
     selector: 'signup-input',
@@ -11,14 +21,77 @@ import {InputType} from "../ss-input/models/InputType";
 })
 export class SignupInputComponent {
     @Input() inputType: AuthInputType;
+    @Input() signupCredentials: SignupCredentials;
+    @Output() inputEvent: EventEmitter<void> = new EventEmitter<void>();
+    @ViewChild(SsInputComponent) inputElement: SsInputComponent;
     constructor() {
         
     }
+
+    protected emitInputEvent() {
+        this.inputEvent.emit();
+    }
+
+    protected sanitizeInput(inputValue: string) {
+        // FIXME: Email a handling doesn't work.
+        if (this.inputType === AuthInputType.EMAIL) {
+            console.log(inputValue)
+            return inputValue.replace(' ', '');
+        }
+        return inputValue.replace(/\s+/g, '');
+    }
+
+    protected handleInput(inputValue: string) {
+        const sanitizedInput = this.sanitizeInput(inputValue);
+        this.setInputValue(sanitizedInput);
+        switch (this.inputType) {
+            case AuthInputType.USERNAME:
+                this.setUsername(sanitizedInput);
+                break;
+            case AuthInputType.PASSWORD:
+                this.setPassword(sanitizedInput);
+                break;
+            case AuthInputType.CONFIRM_PASSWORD:
+                this.setConfirmPassword(sanitizedInput);
+                break;
+            case AuthInputType.EMAIL:
+                this.setEmail(sanitizedInput);
+                break;
+        }
+        this.emitInputEvent();
+    }
+
+    protected setInputValue(sanitizedInput: string) {
+        this.inputElement.value = sanitizedInput;
+    }
+
+    protected setUsername(username: string) {
+        this.signupCredentials.username = username;
+    }
+
+    protected setPassword(password: string) {
+        this.signupCredentials.password = password;
+    }
+
+    protected setConfirmPassword(confirmPassword: string) {
+        this.signupCredentials.confirmPassword = confirmPassword;
+    }
+
+    protected setEmail(email: string) {
+        this.signupCredentials.email = email;
+    }
+
+    protected setTermsAgreed(termsAgreed: boolean) {
+        this.signupCredentials.termsAgreed = termsAgreed;
+        this.emitInputEvent();
+    }
+
     protected isUsernameType(): boolean {
         return this.inputType === AuthInputType.USERNAME;
     }
     protected isPasswordType(): boolean {
-        return this.inputType === AuthInputType.PASSWORD;
+        return this.inputType === AuthInputType.PASSWORD ||
+               this.inputType === AuthInputType.CONFIRM_PASSWORD;
     }
     protected isEmailType(): boolean {
         return this.inputType === AuthInputType.EMAIL;
@@ -28,6 +101,17 @@ export class SignupInputComponent {
     }
     protected getTitleText(): string {
         return this.inputType + ":";
+    }
+
+    protected getPlaceholder(): string {
+        switch (this.inputType) {
+            // case AuthInputType.USERNAME:
+            //     return 'Ex. mike.demarcus1999';
+            // case AuthInputType.EMAIL:
+            //     return 'Ex. mike.demarcus1999@gmail.com';
+            default:
+                return '';
+        }
     }
 
     protected getInputType(): InputType {
