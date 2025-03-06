1d023ec32ee56fda534dc0221be97911ff24e01a
theoliverlear <sigwartholiver2@gmail.com>
2025-03-06 10:30:05 -0600

    Add inputs, outputs, and method for handling login credentials in login-inputs.component.ts

--- angular/components/elements/login-inputs/login-inputs.component.ts
+++ angular/components/elements/login-inputs/login-inputs.component.ts
@@ -1,5 +1,9 @@
 // login-inputs.component.ts 
-import { Component } from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {AuthInputType} from "../../../models/auth/AuthInputType";
+import {
+    LoginCredentials
+} from "../../../models/auth/credentials/LoginCredentials";
 
 @Component({
     selector: 'login-inputs',
@@ -7,7 +11,15 @@ import { Component } from "@angular/core";
     styleUrls: ['./login-inputs.component.css']
 })
 export class LoginInputsComponent {
+    @Input() loginCredentials: LoginCredentials = new LoginCredentials();
+    @Output() credentialsChange: EventEmitter<LoginCredentials> = new EventEmitter<LoginCredentials>();
     constructor() {
         
     }
+
+    protected emitCredentialsChange(): void {
+        this.credentialsChange.emit(this.loginCredentials);
+    }
+
+    protected readonly AuthInputType = AuthInputType;
 }
