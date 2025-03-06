ec45d5d3ab418bcab27f4b2fb6c5b926d41bebdc
theoliverlear <sigwartholiver2@gmail.com>
2025-03-05 19:51:03 -0600

    Add session_service.py to manage user session operations

---
+++ backend/apps/services/session_service.py
@@ -0,0 +1,16 @@
+from backend.apps.entity.user.user import User
+
+
+class SessionService:
+    def remove_user_from_session(self, http_request):
+        if self.user_in_session(http_request):
+            del http_request.session['user_id']
+
+    def save_user_to_session(self, user: User, http_request) -> None:
+        http_request.session['user_id'] = user.id
+
+    def user_in_session(self, http_request) -> bool:
+        return 'user_id' in http_request.session
+
+    def get_session(self, http_request):
+        return http_request.session
