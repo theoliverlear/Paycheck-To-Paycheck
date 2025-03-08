d2616ec39bec912fdb114d2222525b49d32a66bc
theoliverlear <sigwartholiver2@gmail.com>
2025-03-08 15:23:03 -0600

    Change WebSocketService generic type to accept an additional parameter in login-websocket.service.ts

--- angular/services/server/websocket/login-websocket.service.ts
+++ angular/services/server/websocket/login-websocket.service.ts
@@ -3,11 +3,14 @@ import {WebSocketService} from "./websocket.service";
 import {
     LoginCredentials
 } from "../../../models/auth/credentials/LoginCredentials";
+import {
+    AuthResponse
+} from "../../../components/elements/auth-console/models/types";
 
 @Injectable({
     providedIn: 'root'
 })
-export class LoginWebSocketService extends WebSocketService<LoginCredentials> {
+export class LoginWebSocketService extends WebSocketService<LoginCredentials, AuthResponse> {
     private static readonly URL: string = 'ws://localhost:8001/ws/login';
     constructor() {
         super(LoginWebSocketService.URL);
