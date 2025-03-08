19cae6bb58eda6e75280b9cb0113c5c1859dbd68
theoliverlear <sigwartholiver2@gmail.com>
2025-03-08 15:22:42 -0600

    Change WebSocketService generic type to accept an additional parameter in income-websocket.service.ts

--- angular/services/server/websocket/income-websocket.service.ts
+++ angular/services/server/websocket/income-websocket.service.ts
@@ -5,7 +5,7 @@ import {WebSocketService} from "./websocket.service";
 @Injectable({
     providedIn: 'root'
 })
-export class IncomeWebSocketService extends WebSocketService<Income> {
+export class IncomeWebSocketService extends WebSocketService<Income, any> {
     private static readonly URL: string = 'ws://localhost:8001/ws/income';
     constructor() {
         super(IncomeWebSocketService.URL);
