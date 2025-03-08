c34d47dbb03457afbd069935a6be8a02ce47c6c1
theoliverlear <sigwartholiver2@gmail.com>
2025-03-08 15:40:33 -0600

    Refactor income_consumer.py to use dependency injected services

--- backend/apps/routing/websocket/income/income_consumer.py
+++ backend/apps/routing/websocket/income/income_consumer.py
@@ -1,35 +1,39 @@
 import json
+import logging
 
 from asgiref.sync import sync_to_async
+from djangorestframework_camel_case.util import camelize, underscoreize
+from injector import inject
 
 from backend.apps.comm.serialize.entity.income.one_time_income_serializer import \
-    IncomeSerializer
+    OneTimeIncomeSerializer
 from backend.apps.entity.income.one_time_income import OneTimeIncome
+from backend.apps.injector.injectable import injectable
+from backend.apps.models.dict.entity.income.one_time_income_dict_parser import \
+    OneTimeIncomeDictParser
 from backend.apps.routing.websocket.websocket_consumer import \
     WebSocketConsumer
 
-
+@injectable
 class IncomeConsumer(WebSocketConsumer[OneTimeIncome]):
+    @inject
+    def __init__(self, one_time_income_parser: OneTimeIncomeDictParser):
+        super().__init__()
+        self.one_time_income_parser: OneTimeIncomeDictParser = one_time_income_parser
+
     async def receive(self, text_data=None, bytes_data=None) -> None:
         if text_data:
             data = json.loads(text_data)
             income: OneTimeIncome = self.get_income(data)
             await sync_to_async(income.save)()
-            income_json = self.get_income_from_json(data)
+            income_json = self.get_serialized_income(income)
             await self.send(text_data=json.dumps({
                 'message': income_json
             }))
 
-    def get_income_from_json(self, json_data):
-        json_income = self.get_income(json_data)
-        serializer: IncomeSerializer = IncomeSerializer(json_income)
-        return serializer.data
+    def get_serialized_income(self, income: OneTimeIncome):
+        serializer: OneTimeIncomeSerializer = OneTimeIncomeSerializer(income)
+        return camelize(serializer.data)
 
     def get_income(self, json_data):
-        json_name = json_data['title']
-        json_income_amount = json_data['amount']
-        income: OneTimeIncome = OneTimeIncome(
-            name=json_name,
-            income_amount=json_income_amount
-        )
-        return income
+        return self.one_time_income_parser.get_one_time_income(underscoreize(json_data))
