04defa3773174c8ad99068d57516bfa6da25a1fc
theoliverlear <sigwartholiver2@gmail.com>
2025-03-08 15:39:09 -0600

    Refactor bill_consumer.py to use dependency injected services

--- backend/apps/routing/websocket/bill/bill_consumer.py
+++ backend/apps/routing/websocket/bill/bill_consumer.py
@@ -3,6 +3,7 @@ import logging
 from typing import override
 
 from asgiref.sync import sync_to_async
+from djangorestframework_camel_case.util import camelize
 from injector import inject, Injector
 
 from backend.apps.comm.serialize.entity.bill.one_time_bill_serializer import \
@@ -19,16 +20,7 @@ class BillConsumer(WebSocketConsumer[OneTimeBill]):
     @inject
     def __init__(self, one_time_bill_parser: OneTimeBillDictParser):
         super().__init__()
-        self.one_time_bill_parser = one_time_bill_parser
-
-    @override
-    async def connect(self):
-        logging.info('WebSocket connected')
-        await self.accept()
-
-    @override
-    async def disconnect(self, close_code):
-        logging.info('WebSocket disconnected')
+        self.one_time_bill_parser: OneTimeBillDictParser = one_time_bill_parser
 
     @override
     async def receive(self, text_data=None, bytes_data=None):
@@ -44,15 +36,7 @@ class BillConsumer(WebSocketConsumer[OneTimeBill]):
 
     def get_serialized_bill(self, bill: OneTimeBill) -> dict:
         serializer: OneTimeBillSerializer = OneTimeBillSerializer(bill)
-        return serializer.data
-
-    def get_bill_from_json(self, json_data):
-        json_bill = self.get_bill(json_data)
-        # TODO: See if bill already exists or not and implement ID
-        #       accordingly.
-        serializer: OneTimeBillSerializer = OneTimeBillSerializer(json_bill)
-        return serializer.data
-        # return json_bill.model_dump_json()
+        return camelize(serializer.data)
 
     def get_bill(self, json_data):
         return self.one_time_bill_parser.get_one_time_bill(json_data)
