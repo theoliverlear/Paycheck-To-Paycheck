931cd2ee246bcff5062cb353bd6791b5bb17247c
theoliverlear <sigwartholiver2@gmail.com>
2025-03-08 15:31:01 -0600

    Add binding for `OneTimeIncomeDictParser` and remove commented out code in app_module.py

--- backend/apps/injector/app_module.py
+++ backend/apps/injector/app_module.py
@@ -5,13 +5,15 @@ from injector import Binder, singleton, Module
 from backend.apps.comm.request.login_request import LoginRequest
 from backend.apps.comm.request.signup_request import SignupRequest
 from backend.apps.entity.bill.one_time_bill import OneTimeBill
-from backend.apps.entity.income.income_history import IncomeHistory
+from backend.apps.entity.income.one_time_income import OneTimeIncome
 from backend.apps.models.dict.class_field_parser import ClassFieldParser
 from backend.apps.models.dict.class_field_parser_provider import \
     ClassDictParserProvider
 from backend.apps.models.dict.dict_parser import DictParser
 from backend.apps.models.dict.entity.bill.one_time_bill_dict_parser import \
     OneTimeBillDictParser
+from backend.apps.models.dict.entity.income.one_time_income_dict_parser import \
+    OneTimeIncomeDictParser
 from backend.apps.repository.user_repository import UserRepository
 from backend.apps.routing.websocket.bill.bill_consumer import BillConsumer
 from backend.apps.services.auth_service import AuthService
@@ -32,27 +34,12 @@ class AppModule(Module):
 
         binder.bind(DictParser, to=DictParser, scope=singleton)
         binder.bind(OneTimeBillDictParser, to=OneTimeBillDictParser, scope=singleton)
+        binder.bind(OneTimeIncomeDictParser, to=OneTimeIncomeDictParser, scope=singleton)
 
         binder.bind(ClassFieldParser, to=ClassFieldParser, scope=singleton)
         binder.bind(ClassFieldParser[OneTimeBill], to=ClassDictParserProvider(OneTimeBill), scope=singleton)
+        binder.bind(ClassFieldParser[OneTimeIncome], to=ClassDictParserProvider(OneTimeIncome), scope=singleton)
         binder.bind(ClassFieldParser[SignupRequest], to=ClassDictParserProvider(SignupRequest), scope=singleton)
         binder.bind(ClassFieldParser[LoginRequest], to=ClassDictParserProvider(LoginRequest), scope=singleton)
 
         binder.bind(WebSocketSessionService, to=WebSocketSessionService, scope=singleton)
-        # self.bind_dict_parsers(binder)
-        # self.bind_class_dict_parsers(binder)
-
-
-
-    # def bind_dict_parsers(self, binder: Binder):
-    #     binder.bind(DictParser, to=DictParser, scope=singleton)
-    #     dict_parsers = [OneTimeBillDictParser]
-    #     for dict_parser in dict_parsers:
-    #         binder.bind(dict_parser, to=dict_parser, scope=singleton)
-    #
-    #
-    # def bind_class_dict_parsers(self, binder: Binder):
-    #     binder.bind(ClassDictParser, to=ClassDictParser, scope=singleton)
-    #     parsable_classes = [OneTimeBill]
-    #     for parsable_class in parsable_classes:
-    #         binder.bind(ClassDictParser[parsable_class], to=ClassDictParser(parsable_class), scope=singleton)
