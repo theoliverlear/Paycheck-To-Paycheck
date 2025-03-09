79c34f45d4c9ad045ad140f6224a296d67d358df
theoliverlear <sigwartholiver2@gmail.com>
2025-03-09 16:58:10 -0500

    Add recurring_income_serializer.py to serialize RecurringIncome objects

---
+++ backend/apps/comm/serialize/entity/income/recurring_income_serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from backend.apps.comm.serialize.entity.time.recurring_date_serializer import \
+    RecurringDateSerializer
+from backend.apps.entity.income.recurring_income import RecurringIncome
+
+
+class RecurringIncomeSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    name = serializers.CharField()
+    income_amount = serializers.FloatField()
+    recurring_date = RecurringDateSerializer()
+    yearly_income = serializers.FloatField()
+    class Meta:
+        model = RecurringIncome
+        fields = '__all__'
