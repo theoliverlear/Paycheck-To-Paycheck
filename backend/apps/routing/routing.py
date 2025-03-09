160335b6fee3639e6538a374655d599a6d6bf08b
theoliverlear <sigwartholiver2@gmail.com>
2025-03-09 17:17:40 -0500

    Update routing.py to include recurring income WebSocket consumer and rename income consumer classes

--- backend/apps/routing/routing.py
+++ backend/apps/routing/routing.py
@@ -1,18 +1,21 @@
-from django.urls import re_path, URLPattern, URLResolver
+from django.urls import re_path
 from injector import Injector
 
 from backend.apps.injector import AppModule
 from backend.apps.routing.websocket.auth.login_consumer import LoginConsumer
 from backend.apps.routing.websocket.auth.signup_consumer import SignupConsumer
-from backend.apps.routing.websocket.bill.bill_consumer import BillConsumer
-from backend.apps.routing.websocket.income.income_consumer import \
-    IncomeConsumer
+from backend.apps.routing.websocket.bill.one_time_bill_consumer import BillConsumer
+from backend.apps.routing.websocket.bill.recurring_income_consumer import \
+    RecurringIncomeConsumer
+from backend.apps.routing.websocket.income.one_time_income_consumer import \
+    OneTimeIncomeConsumer
 
-injector = Injector(AppModule())
+injector: Injector = Injector(AppModule())
 
 websocket_url_patterns = [
     re_path('ws/bill', BillConsumer.factory(injector)),
-    re_path('ws/income', IncomeConsumer.factory(injector)),
+    re_path('ws/income', OneTimeIncomeConsumer.factory(injector)),
+    re_path('ws/recurring-income', RecurringIncomeConsumer.factory(injector)),
     re_path('ws/signup', SignupConsumer.factory(injector)),
     re_path('ws/login', LoginConsumer.factory(injector))
 ]
