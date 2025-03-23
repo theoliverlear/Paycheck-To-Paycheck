dccc5f5a4250c00ad764ea8dec22cd639ca6fff9
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 19:22:33 -0500

    Add paycheck.component.ts to be an Angular component for the paycheck element in the Angular application

---
+++ angular/components/elements/paycheck/paycheck.component.ts
@@ -0,0 +1,13 @@
+// paycheck.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'paycheck',
+    templateUrl: './paycheck.component.html',
+    styleUrls: ['./paycheck.component.css']
+})
+export class PaycheckComponent {
+    constructor() {
+        
+    }
+}
