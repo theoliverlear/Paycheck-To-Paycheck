8d5b434fb5c1f93ef04c5a31aa69db79853b3022
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 19:31:44 -0500

    Add paycheck-pay-period.component.ts to be an Angular component for the paycheck-pay-period element in the Angular application

---
+++ angular/components/elements/paycheck-pay-period/paycheck-pay-period.component.ts
@@ -0,0 +1,21 @@
+// paycheck-pay-period.component.ts 
+import {Component, Input} from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+
+@Component({
+    selector: 'paycheck-pay-period',
+    templateUrl: './paycheck-pay-period.component.html',
+    styleUrls: ['./paycheck-pay-period.component.css']
+})
+export class PaycheckPayPeriodComponent {
+    @Input() protected startDate: Date;
+    @Input() protected endDate: Date;
+    constructor() {
+        
+    }
+
+    protected getPayPeriodText(): string {
+        return this.startDate + ' - ' + this.endDate;
+    }
+    protected readonly TagType = TagType;
+}
