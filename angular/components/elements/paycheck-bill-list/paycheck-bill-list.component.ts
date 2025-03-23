6c2f057675ec901485beef6add07f052da17aae7
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 19:25:33 -0500

    Add paycheck-bill-list.component.ts to be an Angular component for the paycheck-bill-list element in the Angular application

---
+++ angular/components/elements/paycheck-bill-list/paycheck-bill-list.component.ts
@@ -0,0 +1,13 @@
+// paycheck-bill-list.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'paycheck-bill-list',
+    templateUrl: './paycheck-bill-list.component.html',
+    styleUrls: ['./paycheck-bill-list.component.css']
+})
+export class PaycheckBillListComponent {
+    constructor() {
+        
+    }
+}
