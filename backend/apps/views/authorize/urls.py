80a3769abb7760c2f5d22e888e7b56c560203f1c
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 15:16:23 -0500

    Add URL routes for login, signup, and logged in status views in urls.py

--- backend/apps/views/authorize/urls.py
+++ backend/apps/views/authorize/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = [
+from django.urls import path
 
+from backend.apps.views.authorize.is_logged_in_view import IsLoggedInView
+from backend.apps.views.authorize.login_view import LoginView
+from backend.apps.views.authorize.signup_view import SignupView
+
+urlpatterns = [
+    path('login', LoginView.as_view(), name="login"),
+    path('signup', SignupView.as_view(), name="signup"),
+    path('isloggedin', IsLoggedInView.as_view(), name="isloggedin")
 ]
