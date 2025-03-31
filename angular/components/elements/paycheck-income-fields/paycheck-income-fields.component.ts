3e658bba596f8eaede410f4a337d7ec8fb7bb905
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 17:27:56 -0500

    Add paycheck-income-fields.component.ts to be an Angular component for the paycheck-income-fields element in the Angular application

---
+++ angular/components/elements/paycheck-income-fields/paycheck-income-fields.component.ts
@@ -0,0 +1,20 @@
+// paycheck-income-fields.component.ts 
+import {Component, Input} from "@angular/core";
+import {Income} from "../../../models/income/Income";
+import {
+    PaycheckIncomeFieldType
+} from "../paycheck-income-field/models/PaycheckIncomeFieldType";
+
+@Component({
+    selector: 'paycheck-income-fields',
+    templateUrl: './paycheck-income-fields.component.html',
+    styleUrls: ['./paycheck-income-fields.component.css']
+})
+export class PaycheckIncomeFieldsComponent {
+    @Input() protected income: Income;
+    constructor() {
+        
+    }
+
+    protected readonly PaycheckIncomeFieldType = PaycheckIncomeFieldType;
+}
