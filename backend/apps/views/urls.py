4b9658fa7c08f426c5d3aa666f901c0d06f04ca8
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 15:15:27 -0500

    Add route for '/authorize' in urls.py to include authorization URLs

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -4,6 +4,7 @@ from backend.apps.views.home_view import HomeView
 
 urlpatterns = [
     path('<str:param>', HomeView.as_view(), name='home'),
+    path('authorize/', include('backend.apps.views.authorize.urls')),
     path('paycheck/', include('backend.apps.views.paycheck.urls')),
     path('user/', include('backend.apps.views.user.urls')),
 ]
