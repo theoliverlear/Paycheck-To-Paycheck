4826bb6f2f514881bca2e8a09237f2679532c312
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 17:04:39 -0500

    Add Paycheck.ts to define a paycheck model with date ranges, incomes, bills, and totals

---
+++ angular/models/paycheck/Paycheck.ts
@@ -0,0 +1,24 @@
+import {Income} from "../income/Income";
+import {Bill} from "../bill/Bill";
+
+export class Paycheck {
+    public startDate: Date;
+    public endDate: Date;
+    public incomes: Income[];
+    public bills: Bill[];
+    public totalIncome: number;
+    public totalBills: number;
+    public constructor(startDate: Date = new Date(),
+                       endDate: Date = new Date(),
+                       incomes: Income[] = [],
+                       bills: Bill[] = [],
+                       totalIncome: number = 0.0,
+                       totalBills: number = 0.0) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.incomes = incomes;
+        this.bills = bills;
+        this.totalIncome = totalIncome;
+        this.totalBills = totalBills;
+    }
+}
