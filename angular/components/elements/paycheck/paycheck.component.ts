4842a97aeb771eae56164b18b4fd86dcf42c2f99
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 17:14:02 -0500

    Add sample paycheck data in paycheck.component.ts for development templating

--- angular/components/elements/paycheck/paycheck.component.ts
+++ angular/components/elements/paycheck/paycheck.component.ts
@@ -1,13 +1,54 @@
 // paycheck.component.ts 
-import { Component } from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
+import {Paycheck} from "../../../models/paycheck/Paycheck";
+import {Income} from "../../../models/income/Income";
+import {InputTimeType} from "../../../models/input/InputTimeType";
+import {Bill} from "../../../models/bill/Bill";
+import {PaycheckTotalType} from "../paycheck-total/models/PaycheckTotalType";
 
 @Component({
     selector: 'paycheck',
     templateUrl: './paycheck.component.html',
     styleUrls: ['./paycheck.component.css']
 })
-export class PaycheckComponent {
+export class PaycheckComponent implements OnInit {
+    @Input() protected paycheck: Paycheck = new Paycheck()
     constructor() {
         
     }
+    ngOnInit(): void {
+        this.paycheck.incomes = [new Income(
+            "Job at Target",
+            540,
+            new Date(),
+            InputTimeType.RECURRING
+        ),
+        new Income(
+            "Birthday Money",
+            100,
+            new Date(),
+            InputTimeType.ONE_TIME
+        )];
+        this.paycheck.bills = [new Bill(
+            "Rent",
+            1200,
+            new Date(),
+            InputTimeType.RECURRING
+        ),
+        new Bill(
+            "Car Payment",
+            300,
+            new Date(),
+            InputTimeType.RECURRING
+        ),
+        new Bill(
+            "Haircut",
+            38,
+            new Date(),
+            InputTimeType.RECURRING
+        )];
+        this.paycheck.totalIncome = 1550.65;
+    }
+
+    protected readonly PaycheckTotalType = PaycheckTotalType;
 }
