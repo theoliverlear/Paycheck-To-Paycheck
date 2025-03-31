52fb7aa066c301378c982015ec98cd05cf6ba6e0
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 15:11:41 -0500

    Add support for serializing OperationSuccessResponse in payload_status_response_serializer.py

--- backend/apps/comm/serialize/models/http/payload_status_response_serializer.py
+++ backend/apps/comm/serialize/models/http/payload_status_response_serializer.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 
 from backend.apps.comm.response.auth_status_response import AuthStatusResponse
+from backend.apps.comm.response.operation_success_response import \
+    OperationSuccessResponse
 from backend.apps.comm.serialize.comm.response.auth_status_response_serializer import \
     AuthStatusResponseSerializer
+from backend.apps.comm.serialize.comm.response.operation_success_response_serializer import \
+    OperationSuccessResponseSerializer
 from backend.apps.models.http.payload_status_response import \
     PayloadStatusResponse
 
@@ -18,5 +22,8 @@ class PayloadStatusResponseSerializer(serializers.Serializer):
     def get_payload(self, object):
         if isinstance(object.payload, AuthStatusResponse):
             serializer: AuthStatusResponseSerializer = AuthStatusResponseSerializer(object.payload)
+            return serializer.data
+        if isinstance(object.payload, OperationSuccessResponse):
+            serializer: OperationSuccessResponseSerializer = OperationSuccessResponseSerializer(object.payload)
             return serializer.data
         return object.payload
