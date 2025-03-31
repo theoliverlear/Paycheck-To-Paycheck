0a2e109b60b5cfda9389e49c32cc458837bfc3ad
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 15:08:12 -0500

    Update SignupWebSocketService to use WebSocketAuthResponse type in signup-websocket.service.ts

--- angular/services/server/websocket/signup-websocket.service.ts
+++ angular/services/server/websocket/signup-websocket.service.ts
@@ -4,13 +4,13 @@ import {
     SignupCredentials
 } from "../../../models/auth/credentials/SignupCredentials";
 import {
-    AuthResponse
-} from "../../../components/elements/auth-console/models/types";
+    WebSocketAuthResponse
+} from "../../../models/auth/types";
 
 @Injectable({
     providedIn: 'root'
 })
-export class SignupWebSocketService extends WebSocketService<SignupCredentials, AuthResponse> {
+export class SignupWebSocketService extends WebSocketService<SignupCredentials, WebSocketAuthResponse> {
     private static readonly URL: string = 'ws://localhost:8001/ws/signup';
     constructor() {
         super(SignupWebSocketService.URL);
