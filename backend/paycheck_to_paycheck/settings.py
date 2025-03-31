f2028423a95a2950a8636ed3997f385e1afc923d
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 15:14:10 -0500

    Add CORS support by including `corsheaders` app and middleware in settings.py

--- backend/paycheck_to_paycheck/settings.py
+++ backend/paycheck_to_paycheck/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'backend.apps.routing',
     'livereload',
     'django_injector',
-    'channels'
+    'channels',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -90,6 +92,10 @@ TEMPLATES = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
