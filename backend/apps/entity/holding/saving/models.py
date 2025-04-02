e270fe65450a0940d56fb4aa45c1392134d330e8
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:30:09 -0500

    Add `name` field to SavingOrmModel and InterestSavingOrmModel in models.py

--- backend/apps/entity/holding/saving/models.py
+++ backend/apps/entity/holding/saving/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class InterestSavingOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(default="")
     amount = models.FloatField(default=0.0)
     interest_rate = models.FloatField(default=0.0)
     class Meta:
@@ -10,6 +11,7 @@ class InterestSavingOrmModel(models.Model):
 
 class SavingOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(default="")
     amount = models.FloatField(default=0.0)
     class Meta:
         db_table = 'savings'
