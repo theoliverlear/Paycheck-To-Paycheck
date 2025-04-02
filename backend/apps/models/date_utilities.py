85c8b566370053c4132da00482f46b187d5515c6
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:16:11 -0500

    Refactor date_utilities.py to add type annotations and add `get_weeks_before_date()` and `get_weeks_after_date()` functions

--- backend/apps/models/date_utilities.py
+++ backend/apps/models/date_utilities.py
@@ -2,38 +2,35 @@ import logging
 from calendar import monthrange
 from datetime import datetime, date, timedelta
 
+def get_weeks_before_date(start_date: date, num_weeks: int) -> date:
+    return start_date - timedelta(weeks=num_weeks)
 
-def get_next_friday():
+def get_weeks_after_date(start_date: date, num_weeks: int) -> date:
+    return start_date + timedelta(weeks=num_weeks)
+
+def get_next_friday_from_now() -> date:
     today = date.today()
     while today.weekday() != 4:
         today = today.replace(day=today.day + 1)
     return today
 
-def get_second_friday():
-    day = get_next_friday()
+def get_second_friday_from_now() -> date:
+    day: date = get_next_friday_from_now()
     return get_next_week(day)
 
 def get_tomorrow(day: date) -> date:
-    # return day.replace(day=day.day + 1)
-
-    tomorrow = day + timedelta(days=1)
-    logging.info(f'Tomorrow type: {type(tomorrow)}')
+    tomorrow: date = day + timedelta(days=1)
     return tomorrow
 
-def get_next_week(starting_date: date):
+def get_next_week(starting_date: date) -> date:
     next_week_date = starting_date + timedelta(days=7)
-    # return starting_date.replace(day=starting_date.day + 7)
     return next_week_date
 
-def get_next_bi_week(starting_date: date):
-    # next_bi_week_date = starting_date.replace(day=starting_date.day + 14)
-    # logging.info(next_bi_week_date)
-    # print(next_bi_week_date)
-    next_bi_week_date = starting_date + timedelta(days=14)
+def get_next_bi_week(starting_date: date) -> date:
+    next_bi_week_date: date = starting_date + timedelta(days=14)
     return next_bi_week_date
 
-def get_next_month(starting_date: date):
-    # return starting_date.replace(month=starting_date.month + 1)
+def get_next_month(starting_date: date) -> date:
     next_month: int = starting_date.month + 1
     year: int = starting_date.year
     if next_month > 12:
@@ -46,7 +43,7 @@ def get_next_month(starting_date: date):
 def get_days_in_month(month: int, year: int) -> int:
     return monthrange(year, month)[1]
 
-def get_next_bi_month(starting_date: date):
+def get_next_bi_month(starting_date: date) -> date:
     next_month: int = starting_date.month + 1
     year: int = starting_date.year
     if next_month > 12:
@@ -56,7 +53,7 @@ def get_next_bi_month(starting_date: date):
                          starting_date.day)
     return date(year, next_month, valid_day)
 
-def get_next_year(starting_date: date):
+def get_next_year(starting_date: date) -> date:
     return starting_date.replace(year=starting_date.year + 1)
 
 def iso_to_django_date(iso_date: str) -> date:
