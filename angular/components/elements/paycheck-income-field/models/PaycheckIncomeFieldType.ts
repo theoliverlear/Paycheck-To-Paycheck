c2022521d13086fd4ad9c28cdd34372de3a37127
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 13:59:36 -0500

    Rename and simplify PaycheckIncomeFieldType to PaycheckFieldType in PaycheckFieldType.ts to allow for generalized use

--- angular/components/elements/paycheck-income-field/models/PaycheckIncomeFieldType.ts
+++
@@ -1,5 +0,0 @@
-export enum PaycheckIncomeFieldType {
-    AMOUNT = 'Income Amount',
-    DATE_RECEIVED = 'Date Received',
-    TIME_TYPE = 'Time Type'
-}
---
+++ angular/components/elements/paycheck/models/PaycheckFieldType.ts
@@ -0,0 +1,5 @@
+export enum PaycheckFieldType {
+    AMOUNT = 'Amount',
+    DATE = 'Date',
+    TIME_TYPE = 'Time Type'
+}
