37779ba1daf5d446b85044e309a2c86a11d1faf4
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:19:15 -0500

    Refactor variable naming from `income_amount` to `amount` for consistency in undated_income.py

--- backend/apps/entity/income/undated_income.py
+++ backend/apps/entity/income/undated_income.py
@@ -6,12 +6,12 @@ from backend.apps.entity.identifiable import Identifiable
 @define
 class UndatedIncome(Identifiable):
     name: str = attr(default="")
-    _income_amount: float = attr(default=0.0)
+    _amount: float = attr(default=0.0)
 
     @property
-    def income_amount(self):
-        return self._income_amount
+    def amount(self):
+        return self._amount
 
-    @income_amount.setter
-    def income_amount(self, income_amount):
-        self._income_amount = income_amount
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount
