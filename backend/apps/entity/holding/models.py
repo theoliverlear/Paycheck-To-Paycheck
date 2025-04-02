ba2c1d78a88e5ad63aea055fda89a1bba9e597ce
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:31:35 -0500

    Add `name` field and `db_table` metadata to HoldingOrmModel and InterestHoldingOrmModel in models.py

--- backend/apps/entity/holding/models.py
+++ backend/apps/entity/holding/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 class HoldingOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(default="")
     amount = models.FloatField(default=0.0)
+    class Meta:
+        db_table = 'holdings'
 
 
 class InterestHoldingOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(default="")
     amount = models.FloatField(default=0.0)
     interest_rate = models.FloatField(default=0.0)
+    class Meta:
+        db_table = 'interest_holdings'
