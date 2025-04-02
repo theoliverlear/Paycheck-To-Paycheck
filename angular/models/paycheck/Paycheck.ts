4b98aa104297df35b8b48a88757459219cb9f2cb
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 13:57:41 -0500

    Add left over income property and update constructor in Paycheck.ts

--- angular/models/paycheck/Paycheck.ts
+++ angular/models/paycheck/Paycheck.ts
@@ -8,17 +8,20 @@ export class Paycheck {
     public bills: Bill[];
     public totalIncome: number;
     public totalBills: number;
+    public leftOverIncome: number;
     public constructor(startDate: Date = new Date(),
                        endDate: Date = new Date(),
                        incomes: Income[] = [],
                        bills: Bill[] = [],
                        totalIncome: number = 0.0,
-                       totalBills: number = 0.0) {
+                       totalBills: number = 0.0,
+                       leftOverIncome: number = 0.0) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.incomes = incomes;
         this.bills = bills;
         this.totalIncome = totalIncome;
         this.totalBills = totalBills;
+        this.leftOverIncome = leftOverIncome;
     }
 }
