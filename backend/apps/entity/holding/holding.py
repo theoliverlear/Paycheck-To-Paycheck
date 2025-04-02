2ec4bc1a2aebebe0f68dc5d6fe72cd71f6e74c85
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:32:17 -0500

    Add `name` attribute and `wallet` association in holding.py

--- backend/apps/entity/holding/holding.py
+++ backend/apps/entity/holding/holding.py
@@ -4,7 +4,11 @@ from attr import attr
 from attrs import define
 
 from backend.apps.entity.identifiable import Identifiable
+from backend.apps.entity.wallet.wallet import Wallet
+
 
 @define
 class Holding(ABC, Identifiable):
+    name: str = attr(default="")
     amount: float = attr(default=0.0)
+    wallet: Wallet = attr(factory=Wallet)
