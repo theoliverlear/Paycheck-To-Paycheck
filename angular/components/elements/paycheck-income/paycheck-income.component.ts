15cb945eeafb19fddc583ba8d73510d9eda1824f
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 14:31:48 -0500

    Remove unused imports and properties to clean up code in paycheck-income.component.ts

--- angular/components/elements/paycheck-income/paycheck-income.component.ts
+++ angular/components/elements/paycheck-income/paycheck-income.component.ts
@@ -1,11 +1,7 @@
 // paycheck-income.component.ts
 import {Component, Input} from "@angular/core";
 import {Income} from "../../../models/income/Income";
-import {InputTimeType} from "../../../models/input/InputTimeType";
 import {TagType} from "../../../models/html/TagType";
-import {
-    PaycheckIncomeFieldType
-} from "../paycheck-income-field/models/PaycheckIncomeFieldType";
 
 @Component({
     selector: 'paycheck-income',
@@ -21,6 +17,4 @@ export class PaycheckIncomeComponent {
         return `$${this.income.incomeAmount}`;
     }
     protected readonly TagType = TagType;
-    protected readonly String = String;
-    protected readonly PaycheckIncomeFieldType = PaycheckIncomeFieldType;
 }
