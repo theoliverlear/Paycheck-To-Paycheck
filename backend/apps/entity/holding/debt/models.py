71a9865ed4ba564eb4f5cfd043fd685135447f00
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:34:06 -0500

    Add 'name' field with default value in DebtOrmModel and InterestDebtOrmModel in models.py

--- backend/apps/entity/holding/debt/models.py
+++ backend/apps/entity/holding/debt/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 class DebtOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(default="")
     amount = models.FloatField(default=0.0)
     class Meta:
         db_table = 'debts'
 
 class InterestDebtOrmModel(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(default="")
     amount = models.FloatField(default=0.0)
     interest_rate = models.FloatField(default=0.0)
     class Meta:
