d13864b8421fe85e36e8dd8898aeea3d1914f6db
theoliverlear <sigwartholiver2@gmail.com>
2025-04-02 18:25:08 -0500

    Simplify variable names and add static method for paycheck range in date_range.py

--- backend/apps/entity/time/date_range.py
+++ backend/apps/entity/time/date_range.py
@@ -10,12 +10,20 @@ from backend.apps.models.date_utilities import get_next_bi_week
 
 @define
 class DateRange(Identifiable):
-    starting_date: date = attr(default=date.today())
-    ending_date: date = attr(default=get_next_bi_week(date.today()))
+    start_date: date = attr(default=date.today())
+    end_date: date = attr(default=get_next_bi_week(date.today()))
 
     def in_range(self, date_to_check: date) -> bool:
-        return self.starting_date <= date_to_check <= self.ending_date
+        return self.start_date <= date_to_check <= self.end_date
 
     def print_in_range(self, date_to_check: date) -> None:
         in_range: bool = self.in_range(date_to_check)
-        logging.info(f'The date range starting {self.starting_date} to {self.ending_date} is in range of {date_to_check}: {in_range}')
+        in_range_data: str = (f'The date range starting {self.start_date} to'
+                              f' {self.end_date} is in range of'
+                              f' {date_to_check}: {in_range}')
+        logging.info(in_range_data)
+
+    @staticmethod
+    def get_paycheck_range(start_date: date):
+        return DateRange(start_date=start_date,
+                         end_date=get_next_bi_week(start_date))
