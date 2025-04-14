e7d8255b5b48173f28ca3fa24292b64afa104013
theoliverlear <sigwartholiver2@gmail.com>
2025-04-14 13:13:21 -0500

    Add wallet URL path in urls.py to include wallet-related endpoints

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('authorize/', include('backend.apps.views.authorize.urls')),
     path('paycheck/', include('backend.apps.views.paycheck.urls')),
     path('user/', include('backend.apps.views.user.urls')),
+    path('wallet/', include('backend.apps.views.wallet.urls'))
 ]
