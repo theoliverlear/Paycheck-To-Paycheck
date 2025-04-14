a8752b1a9c7110174cce2d350bb40a5a21077a1d
theoliverlear <sigwartholiver2@gmail.com>
2025-04-14 16:53:15 -0500

    Remove HomeView from urls.py as its testing functionality is no longer needed

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 
-from backend.apps.views.home_view import HomeView
-
 urlpatterns = [
-    path('<str:param>', HomeView.as_view(), name='home'),
     path('authorize/', include('backend.apps.views.authorize.urls')),
     path('paycheck/', include('backend.apps.views.paycheck.urls')),
     path('user/', include('backend.apps.views.user.urls')),
