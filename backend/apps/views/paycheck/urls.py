29001d440a907f40c40ce8c976d3deeffb809802
theoliverlear <sigwartholiver2@gmail.com>
2025-04-14 16:52:29 -0500

    Edit URL path in urls.py by adding "get/" prefix to the paycheck endpoint

--- backend/apps/views/paycheck/urls.py
+++ backend/apps/views/paycheck/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from backend.apps.views.paycheck.paycheck_view import PaycheckView
 
 urlpatterns = [
-    path("<int:params>",PaycheckView.as_view(), name="paycheck"),
+    path("get/<int:params>",PaycheckView.as_view(), name="paycheck"),
 ]
