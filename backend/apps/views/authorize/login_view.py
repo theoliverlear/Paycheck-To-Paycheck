0424855e430322de67ad951986dffc76b308e369
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 15:12:20 -0500

    Refactor login_view.py to use async login handling with dict parser

--- backend/apps/views/authorize/login_view.py
+++ backend/apps/views/authorize/login_view.py
@@ -1,3 +1,5 @@
+# login_view.py
+from asgiref.sync import async_to_sync
 from injector import inject
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -6,6 +8,10 @@ from backend.apps.comm.request.login_request import LoginRequest
 from backend.apps.comm.response.auth_status_response import AuthStatusResponse
 from backend.apps.comm.serialize.comm.request.login_request_serializer import \
     LoginRequestSerializer
+from backend.apps.comm.serialize.models.http.payload_status_response_serializer import \
+    PayloadStatusResponseSerializer
+from backend.apps.models.dict.comm.request.login_request_dict_parser import \
+    LoginRequestDictParser
 from backend.apps.models.http.payload_status_response import \
     PayloadStatusResponse
 from backend.apps.services.auth_service import AuthService
@@ -14,18 +20,20 @@ from backend.apps.services.auth_service import AuthService
 class LoginView(APIView):
     @inject
     def __init__(self,
-                 auth_service: AuthService):
+                 auth_service: AuthService,
+                 dict_parser: LoginRequestDictParser):
         super().__init__()
-        self.auth_service = auth_service
+        self.auth_service: AuthService = auth_service
+        self.dict_parser: LoginRequestDictParser = dict_parser
+
+    async def async_post(self, http_request, *args, **kwargs):
+        login_request: LoginRequest = self.dict_parser.get_login_request(http_request.data)
+        response: PayloadStatusResponse[AuthStatusResponse] = await self.auth_service.http_login(login_request=login_request, http_request=http_request)
+        response_serializer: PayloadStatusResponseSerializer = PayloadStatusResponseSerializer(response)
+        return Response(response_serializer.data, status=response.status_code)
+
 
     def post(self, http_request, *args, **kwargs):
-        serializer: LoginRequestSerializer = LoginRequestSerializer(data=http_request.data)
-        if serializer.is_valid():
-            login_request: LoginRequest = serializer.get_instance()
-            payload_status: PayloadStatusResponse[AuthStatusResponse] = self.auth_service.login(login_request, http_request)
-            return Response(payload_status.payload,
-                            status=payload_status.status_code)
-        else:
-            return Response(serializer.errors, status=400)
+        return async_to_sync(self.async_post)(http_request, *args, **kwargs)
 
 
