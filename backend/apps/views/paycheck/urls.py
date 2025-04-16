e3cacc6f0680b4248ca7659578dbe9fb5d45e8d7
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 15:19:20 -0500

    Edit URL parameter name from 'params' to 'paycheck_num' in urls.py for clarity

--- backend/apps/views/paycheck/urls.py
+++ backend/apps/views/paycheck/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from backend.apps.views.paycheck.paycheck_view import PaycheckView
 
 urlpatterns = [
-    path("get/<int:params>",PaycheckView.as_view(), name="paycheck"),
+    path("get/<int:paycheck_num>",PaycheckView.as_view(), name="paycheck"),
 ]
