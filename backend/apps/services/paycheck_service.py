8d4f10507965b4b03271629ea624d9471815536a
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 15:03:59 -0500

    Refactor paycheck generation logic in paycheck_service.py to streamline bi-weekly calculations and remove unnecessary RecurringDate dependency

--- backend/apps/services/paycheck_service.py
+++ backend/apps/services/paycheck_service.py
@@ -2,10 +2,9 @@ from datetime import date
 
 from backend.apps.entity.paycheck.paycheck import Paycheck
 from backend.apps.entity.time.date_range import DateRange
-from backend.apps.entity.time.recurring_date import RecurringDate
 from backend.apps.entity.user.user import User
-from backend.apps.models.date_utilities import get_bi_weeks_after_date
-from backend.apps.entity.time.year_interval import YearInterval
+from backend.apps.models.date_utilities import get_bi_weeks_after_date, \
+    get_num_bi_weeks_between_dates
 
 
 class PaycheckService:
@@ -14,14 +13,12 @@ class PaycheckService:
         return paycheck
 
     def get_paycheck_from_now(self, user: User, num_paychecks: int):
-        start_date: date = get_bi_weeks_after_date(date.today(), num_paychecks)
         user_payday: date = user.payday.day
-        user_payday_end_date = get_bi_weeks_after_date(user_payday, 1)
-        date_range: DateRange = DateRange(start_date=user_payday, end_date=user_payday_end_date)
-        recurring_date: RecurringDate = RecurringDate(day=start_date, interval=YearInterval.BI_WEEKLY)
-        while not date_range.in_range(start_date):
-            next_payday: date = recurring_date.get_next_date()
-            date_range = DateRange.get_paycheck_range(next_payday)
+        initial_payday_bi_weeks_gap: int  = get_num_bi_weeks_between_dates(user_payday, date.today())
+        total_paychecks: int = initial_payday_bi_weeks_gap + num_paychecks
+        start_date: date = get_bi_weeks_after_date(user_payday, total_paychecks)
+        user_payday_end_date = get_bi_weeks_after_date(start_date, 1)
+        date_range: DateRange = DateRange(start_date=start_date, end_date=user_payday_end_date)
         paycheck: Paycheck = Paycheck.from_user(user, start_date=date_range.start_date)
         return paycheck
 
