642342ce5342ae77dd86c188beaf10b7f9ebf3e5
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 14:27:49 -0500

    Remove legacy code and URL in login.service.ts to simplify service's responsibilities

--- angular/services/server/login.service.ts
+++ angular/services/server/login.service.ts
@@ -9,27 +9,13 @@ import {catchError, map, Observable} from "rxjs";
     providedIn: 'root'
 })
 export class LoginService {
-    static readonly LOGIN_URL: string = 'http://localhost:8080/api/authorize/login';
-    static readonly IS_LOGGED_IN_URL: string = 'http://localhost:8080/api/auth/isloggedin';
+    static readonly IS_LOGGED_IN_URL: string = 'http://localhost:8000/api/authorize/isloggedin';
     constructor(private http: HttpClient,
                 private errorHandlerService: ErrorHandlerService,
                 private hashPasswordService: HashPasswordService) {
         console.log('LoginService loaded');
     }
-    sendLoginToServer(username: string, unhashedPassword: string): Observable<boolean> {
-        const hashedPassword: string = this.hashPasswordService.hashPassword(unhashedPassword);
-        const loginInfo = {
-            username: username,
-            password: hashedPassword
-        };
-        return this.http.post<{isAuthorized: boolean}>(LoginService.LOGIN_URL, loginInfo, httpOptions)
-            .pipe(
-                map(response => {
-                    return response.isAuthorized;
-                }),
-                catchError(this.errorHandlerService.handleError('loginToServer', false))
-            )
-    }
+
     getIsLoggedInFromServer() {
         return this.http.get<{isAuthorized: boolean}>(LoginService.IS_LOGGED_IN_URL, httpOptions)
             .pipe(
