4a1af57b6517a2d3f8004c353a738f94d794c78d
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 14:20:15 -0500

    Add a test call to paychecks endpoint to test budget updates in budget.component.ts

--- angular/components/pages/budget/budget.component.ts
+++ angular/components/pages/budget/budget.component.ts
@@ -1,5 +1,5 @@
 // budget.component.ts 
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {
     fadeInOutAnimation
 } from "../../animations/animations";
@@ -12,8 +12,33 @@ import {
         fadeInOutAnimation
     ]
 })
-export class BudgetComponent {
+export class BudgetComponent implements OnInit {
     constructor() {
         
+    }
+    ngOnInit() {
+        fetch('http://localhost:8000/api/paycheck/get/0', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }).then(
+            (response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Network response was not ok');
+                }
+            }
+        ).then(
+            (data) => {
+                console.log(data);
+            }
+        ).catch(
+            (error) => {
+                console.error('There was a problem with the fetch operation:', error);
+            }
+        )
     }
 }
