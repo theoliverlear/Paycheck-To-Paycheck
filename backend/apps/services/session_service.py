fd505467ca1a19284452ba61514cc7276e9a5973
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 15:04:28 -0500

    Add dependency injection and async method for retrieving users from session in session_service.py

--- backend/apps/services/session_service.py
+++ backend/apps/services/session_service.py
@@ -1,7 +1,27 @@
+# session_service.py
+import logging
+from typing import Optional
+
+from injector import inject
+
 from backend.apps.entity.user.user import User
+from backend.apps.services.user_service import UserService
 
 
 class SessionService:
+
+    @inject
+    def __init__(self,
+                 user_service: UserService):
+        self.user_service: UserService = user_service
+
+    async def get_user_from_session(self, http_request) -> Optional[User]:
+        if not self.user_in_session(http_request):
+            logging.warning("User ID not found in session.")
+            return None
+        user_id: int = http_request.session['user_id']
+        return await self.user_service.get_by_id(user_id)
+
     def remove_user_from_session(self, http_request):
         if self.user_in_session(http_request):
             del http_request.session['user_id']
@@ -10,7 +30,8 @@ class SessionService:
         http_request.session['user_id'] = user.id
 
     def user_in_session(self, http_request) -> bool:
-        return 'user_id' in http_request.session
+        user_id_key_exists: bool = 'user_id' in http_request.session
+        return user_id_key_exists
 
     def get_session(self, http_request):
         return http_request.session
