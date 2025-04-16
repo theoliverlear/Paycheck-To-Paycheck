2e850f9da87aedfdfde11f600b848f2d6f9af85c
theoliverlear <sigwartholiver2@gmail.com>
2025-04-16 15:14:43 -0500

    Refactor signup logic in signup_view.py to use a dict parser for request parsing and separate async and sync POST methods

--- backend/apps/views/authorize/signup_view.py
+++ backend/apps/views/authorize/signup_view.py
@@ -1,24 +1,36 @@
+# signup_view.py
+from asgiref.sync import async_to_sync
 from injector import inject
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from backend.apps.comm.response.auth_status_response import AuthStatusResponse
 from backend.apps.comm.serialize.comm.request.signup_request_serializer import \
     SignupRequestSerializer
+from backend.apps.comm.serialize.models.http.payload_status_response_serializer import \
+    PayloadStatusResponseSerializer
+from backend.apps.models.dict.comm.request.signup_request_dict_parser import \
+    SignupRequestDictParser
+from backend.apps.models.http.payload_status_response import \
+    PayloadStatusResponse
 from backend.apps.services.auth_service import AuthService
 
 
 class SignupView(APIView):
     @inject
     def __init__(self,
-                 auth_service: AuthService):
+                 auth_service: AuthService,
+                 dict_parser: SignupRequestDictParser):
         super().__init__()
         self.auth_service = auth_service
-    async def post(self, http_request, *args, **kwargs):
-        serializer: SignupRequestSerializer = SignupRequestSerializer(data=http_request.data)
-        if serializer.is_valid():
-            signup_request = serializer.get_instance()
-            payload_status = await self.auth_service.signup(signup_request, http_request)
-            return Response(payload_status.payload,
-                            status=payload_status.status_code)
-        else:
-            return Response(serializer.errors, status=400)
+        self.dict_parser: SignupRequestDictParser = dict_parser
+    async def async_post(self, http_request, *args, **kwargs):
+        signup_request = self.dict_parser.get_signup_request(http_request.data)
+        payload_status: PayloadStatusResponse[AuthStatusResponse] = \
+            await self.auth_service.signup(signup_request, http_request)
+        payload_status_serializer: PayloadStatusResponseSerializer = PayloadStatusResponseSerializer(payload_status)
+        return Response(payload_status_serializer.data,
+                        status=payload_status.status_code)
+
+    def post(self, request, *args, **kwargs):
+        return async_to_sync(self.async_post)(request, *args, **kwargs)
