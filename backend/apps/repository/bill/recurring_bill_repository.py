24b54913cee606e91aeca46f45ff5d0cb19c32e8
theoliverlear <sigwartholiver2@gmail.com>
2025-04-18 07:10:54 -0500

    Add recurring_bill_repository.py to handle recurring bill ORM queries

---
+++ backend/apps/repository/bill/recurring_bill_repository.py
@@ -0,0 +1,24 @@
+from channels.db import database_sync_to_async
+
+from backend.apps.entity.bill.bill_history import BillHistory
+from backend.apps.entity.bill.models import RecurringBillOrmModel, \
+    BillHistoryOrmModel
+from backend.apps.entity.bill.recurring_bill import RecurringBill
+
+
+class RecurringBillRepository:
+    def get_by_id(self, recurring_bill_id: int):
+        # TODO: Probably needs to be async and then needs model instead of ORM
+        #       model.
+        return RecurringBillOrmModel.objects.filter(id=recurring_bill_id).first()
+
+    async def get_all_by_bill_history(self, bill_history: BillHistory) -> list[RecurringBill]:
+        bill_history_orm_model: BillHistoryOrmModel = bill_history.get_orm_model()
+        orm_models: list[RecurringBillOrmModel] = await database_sync_to_async(lambda: list(RecurringBillOrmModel.objects.filter(
+            bill_history=bill_history_orm_model
+        )))()
+        recurring_bills: list[RecurringBill] = []
+        for orm_model in orm_models:
+            recurring_bill: RecurringBill = await database_sync_to_async(RecurringBill.from_orm_model)(orm_model)
+            recurring_bills.append(recurring_bill)
+        return recurring_bills
