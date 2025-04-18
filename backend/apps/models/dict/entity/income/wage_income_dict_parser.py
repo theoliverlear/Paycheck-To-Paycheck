888ee9d19abc253053a275ac1cac65474b391f36
theoliverlear <sigwartholiver2@gmail.com>
2025-04-18 10:02:54 -0500

    Expand wage_income_dict_parser.py to include interval key normalization and use `underscoreize` for camel case to snake case conversion

--- backend/apps/models/dict/entity/income/wage_income_dict_parser.py
+++ backend/apps/models/dict/entity/income/wage_income_dict_parser.py
@@ -1,6 +1,8 @@
 from datetime import date
 
+from djangorestframework_camel_case.util import underscoreize
 from injector import inject
+from typing_extensions import override
 
 from backend.apps.entity.income.wage_income import WageIncome
 from backend.apps.entity.time.recurring_date import RecurringDate
@@ -17,7 +19,25 @@ class WageIncomeDictParser(DictParser):
                  class_dict_parser: ClassFieldParser[WageIncome]):
         self.class_dict_parser: ClassFieldParser[WageIncome] = class_dict_parser
 
+    @override
+    def parse(self, dict_data: dict,
+              key: str) -> str | int | float | date:
+        normalized_key: str = self.camel_to_snake(key)
+        if self.is_interval_key(normalized_key):
+            self.normalize_interval_key(dict_data, normalized_key)
+            return dict_data['interval']
+        else:
+            return super().parse(dict_data, normalized_key)
+
+    def is_interval_key(self, key) -> bool:
+        return key == 'income_interval'
+
+    def normalize_interval_key(self, dict_data: dict, key):
+        dict_value: str = dict_data[key]
+        dict_data['interval'] = dict_value
+
     def get_wage_income(self, dict_data: dict):
+        dict_data = underscoreize(dict_data)
         keys_to_skip: list[str] = ['id', 'income_history', 'recurring_date', 'yearly_income']
         income: dict = {}
         date_received: date = iso_to_django_date(dict_data['date_received'])
