ae03642737bbaebfeb995f2f591ae43eead705d3
theoliverlear <sigwartholiver2@gmail.com>
2025-04-17 19:57:39 -0500

    Add recurring_bill_dict_parser.py for parsing RecurringBill objects from dictionaries

---
+++ backend/apps/models/dict/entity/bill/recurring_bill_dict_parser.py
@@ -0,0 +1,49 @@
+from datetime import date
+
+from injector import inject
+
+from backend.apps.entity.bill.bill_history import BillHistory
+from backend.apps.entity.bill.recurring_bill import RecurringBill
+from backend.apps.entity.time.recurring_date import RecurringDate
+from backend.apps.entity.time.year_interval import YearInterval
+from backend.apps.models.dict.class_field_parser import ClassFieldParser
+from backend.apps.models.dict.dict_parser import DictParser
+
+
+class RecurringBillDictParser(DictParser):
+    @inject
+    def __init__(self, class_dict_parser: ClassFieldParser[RecurringBill]):
+        self.class_dict_parser: ClassFieldParser[RecurringBill] = class_dict_parser
+
+    def get_recurring_bill(self, dict_data: dict) -> RecurringBill:
+        keys_to_skip: list[str] = ['id', 'bill_history', 'bill_interval']
+        bill_dict: dict = {}
+        for key in self.class_dict_parser.get_class_fields():
+            if key not in keys_to_skip:
+                bill_dict[key] = self.parse(dict_data, key)
+                if isinstance(bill_dict[key], date):
+                    bill_interval: YearInterval = self.interval_text_to_enum(dict_data['bill_interval'])
+                    bill_dict[key] = RecurringDate(day=bill_dict[key],
+                                                   interval=bill_interval)
+        recurring_bill: RecurringBill = RecurringBill(**bill_dict)
+        recurring_bill.bill_history = BillHistory()
+        return recurring_bill
+
+
+    def interval_text_to_enum(self, bill_interval: str) -> YearInterval:
+        match bill_interval:
+            case 'Monthly':
+                return YearInterval.MONTHLY
+            case 'Yearly':
+                return YearInterval.YEARLY
+            case 'Bi-Weekly':
+                return YearInterval.BI_WEEKLY
+            case 'Weekly':
+                return YearInterval.WEEKLY
+            case 'Quarterly':
+                return YearInterval.QUARTERLY
+            case 'Daily':
+                return YearInterval.DAILY
+            case _:
+                raise ValueError(f"Invalid interval: {bill_interval}")
+
