c5848f776ac8b421dc82f80e6227c18fea94e14d
theoliverlear <sigwartholiver2@gmail.com>
2025-04-17 19:47:43 -0500

    Add types.ts to define paycheck-related TypeScript models in angular/models/paycheck

---
+++ angular/models/paycheck/types.ts
@@ -0,0 +1,59 @@
+export type DateRange = {
+    endDate: Date;
+    startDate: Date;
+};
+
+export type DueDate = {
+    name: string;
+    dueDate: Date;
+};
+
+export type RecurringDate = {
+    day: Date;
+    interval: number;
+}
+
+export type OneTimeBill = {
+    amount: number;
+    dueDate: DueDate;
+    name: string;
+};
+
+export type RecurringBill = {
+    amount: number;
+    recurringDate: RecurringDate;
+    name: string;
+};
+
+export type OneTimeIncome = {
+    amount: number;
+    dateReceived: Date;
+    name: string;
+};
+
+export type RecurringIncome = {
+    amount: number;
+    recurringDate: RecurringDate;
+    name: string;
+    yearlyIncome: number;
+};
+
+export type WageIncome = {
+    amount: number;
+    recurringDate: RecurringDate;
+    name: string;
+    yearlyIncome: number;
+    weeklyHours: number;
+};
+
+export type Paycheck = {
+    dateRange: DateRange;
+    leftOverIncome: number;
+    oneTimeBills: OneTimeBill[];
+    oneTimeIncomes: OneTimeIncome[];
+    recurringBills: RecurringBill[];
+    recurringIncomes: RecurringIncome[];
+    wageIncomes: WageIncome[];
+    totalBills: number;
+    totalIncome: number;
+};
