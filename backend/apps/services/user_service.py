e2ff7a74afeddc78decb07ac9eacbe128cb31732
theoliverlear <sigwartholiver2@gmail.com>
2025-04-18 07:48:31 -0500

    Expand user_service.py to integrate BillHistoryService and IncomeHistoryService for user bill and income history handling

--- backend/apps/services/user_service.py
+++ backend/apps/services/user_service.py
@@ -17,12 +17,20 @@ from backend.apps.entity.income.wage_income import WageIncome
 from backend.apps.entity.user.safe_password import SafePassword
 from backend.apps.entity.user.user import User
 from backend.apps.repository.user_repository import UserRepository
+from backend.apps.services.bill.bill_history_service import BillHistoryService
+from backend.apps.services.income.income_history_service import \
+    IncomeHistoryService
 
 
 class UserService:
     @inject
-    def __init__(self, user_repository: UserRepository):
-        self.user_repository = user_repository
+    def __init__(self,
+                 user_repository: UserRepository,
+                 income_history_service: IncomeHistoryService,
+                 bill_history_service: BillHistoryService):
+        self.user_repository: UserRepository = user_repository
+        self.income_history_service: IncomeHistoryService = income_history_service
+        self.bill_history_service: BillHistoryService = bill_history_service
 
     def user_from_request(self, user_request: SignupRequest | LoginRequest) -> User:
         user: User = User(
@@ -37,13 +45,17 @@ class UserService:
         return await database_sync_to_async(self.user_repository.get_by_username)(username)
 
     async def get_by_id(self, user_id: int):
+        # user: User = await database_sync_to_async(self.user_repository.get_by_id)(user_id)
+        # bill_history = await self.get_user_bill_history(user)
+        # income_history = await self.get_user_income_history(user)
+        #
+        # user.user_bill_history = bill_history
+        # user.user_income_history = income_history
         user: User = await database_sync_to_async(self.user_repository.get_by_id)(user_id)
-        bill_history = await self.get_user_bill_history(user)
-        income_history = await self.get_user_income_history(user)
-
+        bill_history: BillHistory = await self.bill_history_service.initialize_bill_history(user)
+        income_history: IncomeHistory = await self.income_history_service.initialize_income_history(user)
         user.user_bill_history = bill_history
         user.user_income_history = income_history
-
         return user
 
     async def get_user_income_history(self, user: User):
