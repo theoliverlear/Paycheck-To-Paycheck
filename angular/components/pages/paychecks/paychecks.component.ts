a13990f8657e75080edda966eed12851e93d5c30
theoliverlear <sigwartholiver2@gmail.com>
2025-04-20 17:47:25 -0500

    Add input properties and `OnInit` lifecycle to dynamically initialize `paycheckIds` in paychecks.component.ts

--- angular/components/pages/paychecks/paychecks.component.ts
+++ angular/components/pages/paychecks/paychecks.component.ts
@@ -1,13 +1,21 @@
 // paychecks.component.ts 
-import { Component } from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 
 @Component({
     selector: 'paychecks',
     templateUrl: './paychecks.component.html',
     styleUrls: ['./paychecks.component.css']
 })
-export class PaychecksComponent {
+export class PaychecksComponent implements OnInit {
+    @Input() numPaychecks: number = 5;
+    @Input() paycheckIds: number[] = [];
     constructor() {
         
+    }
+
+    ngOnInit(): void {
+        for (let i = 0; i < this.numPaychecks; i++) {
+            this.paycheckIds.push(i);
+        }
     }
 }
