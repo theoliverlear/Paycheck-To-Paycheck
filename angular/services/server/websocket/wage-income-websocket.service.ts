c90bfc01f73f9c4bfa24c57e101463738f29ee8a
theoliverlear <sigwartholiver2@gmail.com>
2025-04-24 08:05:15 -0500

    Update wage-income-websocket.service.ts to use a dynamic URL based on production or development status

--- angular/services/server/websocket/wage-income-websocket.service.ts
+++ angular/services/server/websocket/wage-income-websocket.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from "@angular/core";
 import {WebSocketService} from "./websocket.service";
 import {Income} from "../../../models/income/Income";
+import {environment} from "../../../environments/environment";
 
 
 @Injectable({
     providedIn: 'root'
 })
 export class WageIncomeWebSocketService extends WebSocketService<Income, any> {
-    private static readonly URL: string = 'ws://localhost:8001/ws/wage-income';
+    private static readonly URL: string = `${environment.webSocketUrl}/wage-income`;
     constructor() {
         super(WageIncomeWebSocketService.URL);
     }
