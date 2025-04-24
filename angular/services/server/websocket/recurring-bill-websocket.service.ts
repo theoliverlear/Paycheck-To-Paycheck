cf5a854a48fd8e02048ec879c89328727ca0b527
theoliverlear <sigwartholiver2@gmail.com>
2025-04-24 08:03:50 -0500

    Update recurring-bill-websocket.service.ts to use a dynamic URL based on production or development status

--- angular/services/server/websocket/recurring-bill-websocket.service.ts
+++ angular/services/server/websocket/recurring-bill-websocket.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from "@angular/core";
 import {WebSocketService} from "./websocket.service";
 import {Bill} from "../../../models/bill/Bill";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RecurringBillWebSocketService extends WebSocketService<Bill, any> {
-    private static readonly URL: string = 'ws://localhost:8001/ws/recurring-bill';
+    private static readonly URL: string = `${environment.webSocketUrl}/recurring-bill`;
     constructor() {
         super(RecurringBillWebSocketService.URL);
     }
