520e8f0a7305714244144166afb0241797f06a9c
theoliverlear <sigwartholiver2@gmail.com>
2025-04-24 08:04:10 -0500

    Update recurring-income-websocket.service.ts to use a dynamic URL based on production or development status

--- angular/services/server/websocket/recurring-income-websocket.service.ts
+++ angular/services/server/websocket/recurring-income-websocket.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from "@angular/core";
 import {WebSocketService} from "./websocket.service";
 import {Income} from "../../../models/income/Income";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RecurringIncomeWebSocketService extends WebSocketService<Income, any> {
-    private static readonly URL: string = 'ws://localhost:8001/ws/recurring-income';
+    private static readonly URL: string = `${environment.webSocketUrl}/recurring-income`;
     constructor() {
         super(RecurringIncomeWebSocketService.URL);
     }
