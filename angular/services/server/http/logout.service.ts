068b91606a274624a64afa6a86e36e4296eb401d
theoliverlear <sigwartholiver2@gmail.com>
2025-04-24 08:07:44 -0500

    Update logout.service.ts to use a dynamic URL based on production or development status

--- angular/services/server/http/logout.service.ts
+++ angular/services/server/http/logout.service.ts
@@ -3,12 +3,13 @@ import {OperationSuccessResponse} from "../../../models/http/types";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class LogoutService extends HttpClientService<any, OperationSuccessResponse> {
-    static readonly LOGOUT_URL: string = 'http://localhost:8000/api/authorize/logout';
+    static readonly LOGOUT_URL: string = `${environment.apiUrl}/authorize/logout`;
 
     constructor(httpClient: HttpClient) {
         super(LogoutService.LOGOUT_URL, httpClient);
