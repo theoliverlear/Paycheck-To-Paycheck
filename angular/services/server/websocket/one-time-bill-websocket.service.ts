fafbac96ad141a61f0876ca9bb35672b65e6eac2
theoliverlear <sigwartholiver2@gmail.com>
2025-04-24 08:03:11 -0500

    Update one-time-bill-websocket.service.ts to use a dynamic URL based on production or development status

--- angular/services/server/websocket/one-time-bill-websocket.service.ts
+++ angular/services/server/websocket/one-time-bill-websocket.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from "@angular/core";
 import {WebSocketService} from "./websocket.service";
 import {Bill} from "../../../models/bill/Bill";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class OneTimeBillWebsocketService extends WebSocketService<Bill, any> {
-    private static readonly URL: string = 'ws://localhost:8001/ws/one-time-bill';
+    private static readonly URL: string = `${environment.webSocketUrl}/one-time-bill`;
     constructor() {
         super(OneTimeBillWebsocketService.URL);
     }
