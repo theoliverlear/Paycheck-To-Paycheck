93db0273a26168a01fbdba618b95400185b32638
theoliverlear <sigwartholiver2@gmail.com>
2025-04-24 08:03:29 -0500

    Update one-time-income-websocket.service.ts to use a dynamic URL based on production or development status

--- angular/services/server/websocket/one-time-income-websocket.service.ts
+++ angular/services/server/websocket/one-time-income-websocket.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from "@angular/core";
 import {Income} from "../../../models/income/Income";
 import {WebSocketService} from "./websocket.service";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class OneTimeIncomeWebSocketService extends WebSocketService<Income, any> {
-    private static readonly URL: string = 'ws://localhost:8001/ws/one-time-income';
+    private static readonly URL: string = `${environment.webSocketUrl}/one-time-income`;
     constructor() {
         super(OneTimeIncomeWebSocketService.URL);
     }
