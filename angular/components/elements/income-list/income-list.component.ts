91680ec3c7a875b46657df781403f2bca433961f
theoliverlear <sigwartholiver2@gmail.com>
2025-04-30 18:57:19 -0500

    Add income fetching in income-list.component.ts

--- angular/components/elements/income-list/income-list.component.ts
+++ angular/components/elements/income-list/income-list.component.ts
@@ -1,5 +1,5 @@
 // income-list.component.ts 
-import {Component, Input, ViewChild} from "@angular/core";
+import {Component, Input, OnInit, ViewChild} from "@angular/core";
 import {TagType} from "../../../models/html/TagType";
 import {ElementSize} from "../../../models/ElementSize";
 import {ButtonText} from "../ss-button/models/ButtonText";
@@ -9,23 +9,32 @@ import {
     WageIncome
 } from "../../../models/paycheck/types";
 import {IncomeInputComponent} from "../income-input/income-input.component";
+import {
+    HttpGetAllIncomesService
+} from "../../../services/server/http/http-get-all-incomes.service";
 
 @Component({
     selector: 'income-list',
     templateUrl: './income-list.component.html',
     styleUrls: ['./income-list.component.css']
 })
-export class IncomeListComponent {
+export class IncomeListComponent implements OnInit {
     @ViewChild(IncomeInputComponent) incomeInput: IncomeInputComponent;
-    @Input() incomes: (OneTimeIncome | RecurringIncome | WageIncome)[] = [
-        // {
-        //     amount: 150,
-        //     dateReceived: new Date(),
-        //     name: "Joe's Birthday Gift",
-        // }
-    ];
-    constructor() {
+    @Input() incomes: (OneTimeIncome | RecurringIncome | WageIncome)[] = [];
+    constructor(private getAllIncomesService: HttpGetAllIncomesService) {
         
+    }
+
+    ngOnInit(): void {
+        this.updateIncomes();
+    }
+
+    public updateIncomes(): void {
+        this.getAllIncomesService.getAllIncomes().subscribe(incomes => {
+            if (incomes) {
+                this.incomes = [...incomes.oneTimeIncomes, ...incomes.recurringIncomes, ...incomes.wageIncomes];
+            }
+        });
     }
 
     public showIncomeInput(): void {
