25e6a15c6565a6dfd4bae69b54ccefc12d557f1b
theoliverlear <sigwartholiver2@gmail.com>
2025-04-30 09:42:25 -0500

    Refactor account.guard.ts to use LoggedInStatusService and improve navigation with returnUrl query parameters

--- angular/services/guard/account.guard.ts
+++ angular/services/guard/account.guard.ts
@@ -4,34 +4,37 @@ import {
     CanActivate, Router,
     RouterStateSnapshot
 } from "@angular/router";
-import {catchError, map, Observable} from "rxjs";
+import {catchError, map, Observable, of} from "rxjs";
 import {LoginService} from "../server/login.service";
+import {HttpAuthResponse} from "../../models/auth/types";
+import {LoggedInStatusService} from "../server/http/logged-in-status.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AccountGuard implements CanActivate {
-    constructor(private loginService: LoginService,
+    constructor(private loggedInStatusService: LoggedInStatusService,
                 private router: Router) {
 
     }
-    canActivate(route: ActivatedRouteSnapshot,
-                state: RouterStateSnapshot): Observable<boolean> {
-        return this.loginService.getIsLoggedInFromServer().pipe(
-            map(isAuthorized => {
-                if (isAuthorized) {
-                    this.router.navigate(['/account']);
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this.loggedInStatusService.isLoggedIn().pipe(
+            map((response: HttpAuthResponse): boolean => {
+                if (response.payload.isAuthorized) {
                     return true;
                 } else {
-                    return false;
+                    this.router.navigate(['/account'], {
+                        queryParams: { returnUrl: state.url } 
+                    });
+                    return false; 
                 }
             }),
-            catchError(() => {
-                this.router.navigate(['/authorize']);
-                return new Observable<boolean>(observer => {
-                    observer.next(false);
+            catchError((error) => {
+                this.router.navigate(['/authorize'], {
+                    queryParams: { returnUrl: state.url } 
                 });
+                return of(false); 
             })
-        )
+        );
     }
 }
