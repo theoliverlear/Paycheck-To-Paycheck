6dfe63465b72566acfbebf21e71844729a5e4ddc
theoliverlear <sigwartholiver2@gmail.com>
2025-04-30 09:43:22 -0500

    Refactor auth.guard.ts to replace LoginService with LoggedInStatusService

--- angular/services/guard/auth.guard.ts
+++ angular/services/guard/auth.guard.ts
@@ -6,33 +6,37 @@ import {
     Router,
     RouterStateSnapshot
 } from "@angular/router";
-import {catchError, map, Observable} from "rxjs";
+import {catchError, map, Observable, of} from "rxjs";
+import {LoggedInStatusService} from "../server/http/logged-in-status.service";
+import {HttpAuthResponse} from "../../models/auth/types";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-    constructor(private loginService: LoginService,
+    constructor(private loggedInStatusService: LoggedInStatusService,
                 private router: Router) {
 
     }
-    canActivate(route: ActivatedRouteSnapshot,
-                state: RouterStateSnapshot): Observable<boolean> {
-        return this.loginService.getIsLoggedInFromServer().pipe(
-            map(isAuthorized => {
-                if (isAuthorized) {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this.loggedInStatusService.isLoggedIn().pipe(
+            map((response: HttpAuthResponse): boolean => {
+                if (response.payload.isAuthorized) {
                     return true;
                 } else {
-                    this.router.navigate(['/authorize']);
-                    return false;
+                    this.router.navigate(['/authorize'], {
+                        queryParams: { returnUrl: state.url } 
+                    });
+                    return false; 
                 }
             }),
-            catchError(() => {
-                this.router.navigate(['/authorize']);
-                return new Observable<boolean>(observer => {
-                    observer.next(false);
+            catchError((error) => {
+                console.error('AuthGuard: Error during API call ->', error);
+                this.router.navigate(['/authorize'], {
+                    queryParams: { returnUrl: state.url } 
                 });
+                return of(false); 
             })
-        )
+        );
     }
 }
