e565929aa8eb51f9a8ca3b49eaa0531de331423f
theoliverlear <sigwartholiver2@gmail.com>
2025-04-30 09:47:22 -0500

    Add LogoutView endpoint to urls.py for handling user logout functionality

--- backend/apps/views/authorize/urls.py
+++ backend/apps/views/authorize/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from backend.apps.views.authorize.is_logged_in_view import IsLoggedInView
 from backend.apps.views.authorize.login_view import LoginView
+from backend.apps.views.authorize.logout_view import LogoutView
 from backend.apps.views.authorize.signup_view import SignupView
 
 urlpatterns = [
     path('login', LoginView.as_view(), name="login"),
     path('signup', SignupView.as_view(), name="signup"),
-    path('isloggedin', IsLoggedInView.as_view(), name="isloggedin")
+    path('isloggedin', IsLoggedInView.as_view(), name="isloggedin"),
+    path('logout', LogoutView.as_view(), name="logout"),
 ]
