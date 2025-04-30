19c1b6909786e1317eec2069dfdd1e571c92e4c8
theoliverlear <sigwartholiver2@gmail.com>
2025-04-30 09:46:48 -0500

    Add synchronous wrapper for async logout and enforce no-cache in logout_view.py

--- backend/apps/views/authorize/logout_view.py
+++ backend/apps/views/authorize/logout_view.py
@@ -1,3 +1,6 @@
+from asgiref.sync import async_to_sync
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from injector import inject
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -19,8 +22,18 @@ class LogoutView(APIView):
         super().__init__()
         self.auth_service: AuthService = auth_service
 
-    async def get(self, http_request, *args, **kwargs):
+    async def async_get(self, http_request, *args, **kwargs):
         payload_status: PayloadStatusResponse[OperationSuccessResponse] = await self.auth_service.logout(CommunicationType.HTTP,
                                                                                                    http_request=http_request)
         serializer: PayloadStatusResponseSerializer = PayloadStatusResponseSerializer(payload_status)
         return Response(serializer.data)
+
+    @method_decorator(never_cache)
+    def get(self, http_request, *args, **kwargs):
+
+        response: Response = async_to_sync(self.async_get)(http_request, *args, **kwargs)
+        response.delete_cookie('devsessionid',
+        path = '/',
+        domain = None)
+        response.delete_cookie('sessionid')
+        return response
