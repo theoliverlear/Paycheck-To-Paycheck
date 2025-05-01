b97939d30e4f83507acb8e08f894852e49492dcb
theoliverlear <sigwartholiver2@gmail.com>
2025-04-30 19:11:39 -0500

    Add URL paths for bill and income views in urls.py

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('authorize/', include('backend.apps.views.authorize.urls')),
+    path('bill/', include('backend.apps.views.bill.urls')),
+    path('income/', include('backend.apps.views.income.urls')),
     path('paycheck/', include('backend.apps.views.paycheck.urls')),
     path('user/', include('backend.apps.views.user.urls')),
     path('wallet/', include('backend.apps.views.wallet.urls'))
