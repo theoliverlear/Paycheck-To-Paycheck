8840defe1cdfa32c5d9a56bd1dbfd192444228df
theoliverlear <sigwartholiver2@gmail.com>
2025-05-09 17:32:13 -0500

    Expand paycheck.component.ts to utilize paycheck assets and add display binding for empty paychecks

--- angular/components/elements/paycheck/paycheck.component.ts
+++ angular/components/elements/paycheck/paycheck.component.ts
@@ -1,5 +1,12 @@
-// paycheck.component.ts 
-import {Component, Input, OnInit} from "@angular/core";
+// paycheck.component.ts
+import {
+    Component,
+    EventEmitter,
+    HostBinding,
+    Input,
+    OnInit,
+    Output
+} from "@angular/core";
 import {PaycheckTotalType} from "../paycheck-total/models/PaycheckTotalType";
 import {
     HttpPaycheckService
@@ -10,6 +17,7 @@ import {
     Paycheck, RecurringBill,
     RecurringIncome, WageIncome
 } from "../../../models/paycheck/types";
+import {emptyPaycheck, isEmptyPaycheck} from "../../../assets/paycheckAssets";
 
 @Component({
     selector: 'paycheck',
@@ -19,34 +27,32 @@ import {
 export class PaycheckComponent implements OnInit {
     // TODO: Create an asset that holds a default paycheck.
     @Input() paycheckId: number = 0;
-    @Input() paycheck: Paycheck = {
-        dateRange: {
-            endDate: new Date(),
-            startDate: new Date()
-        },
-        leftOverIncome: 0,
-        oneTimeBills: [],
-        oneTimeIncomes: [],
-        recurringBills: [],
-        recurringIncomes: [],
-        wageIncomes: [],
-        totalBills: 0,
-        totalIncome: 0
-    };
+    @Input() paycheck: Paycheck = emptyPaycheck;
+    @Output() isLoaded: EventEmitter<number> = new EventEmitter<number>();
+    @HostBinding('style.display') get display(): string {
+        return this.isEmpty() ? 'none' : 'flex';
+    }
+
     incomes: (OneTimeIncome | RecurringIncome | WageIncome)[] = [];
     bills: (OneTimeBill | RecurringBill)[] = [];
 
     constructor(private httpPaycheckService: HttpPaycheckService) {
-        
+
     }
+
     ngOnInit(): void {
         this.httpPaycheckService.getPaycheck(this.paycheckId).subscribe(paycheck => {
             if (paycheck) {
                 this.paycheck = paycheck;
                 this.incomes = [...paycheck.oneTimeIncomes, ...paycheck.recurringIncomes, ...paycheck.wageIncomes];
                 this.bills = [...paycheck.oneTimeBills, ...paycheck.recurringBills];
+                this.isLoaded.emit(this.paycheckId);
             }
         });
+    }
+
+    isEmpty(): boolean {
+        return isEmptyPaycheck(this.paycheck);
     }
 
     protected readonly PaycheckTotalType = PaycheckTotalType;
