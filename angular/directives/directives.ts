6f76312c0ea3eec4c70af600b4264580ff7bc337
theoliverlear <sigwartholiver2@gmail.com>
2025-05-09 17:35:31 -0500

    Add BlurOnVisibilityDirective to directives.ts for handling element visibility-based blur functionality

--- angular/directives/directives.ts
+++ angular/directives/directives.ts
@@ -1,7 +1,9 @@
 import {UndraggableDirective} from "./undraggable.directive";
 import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
+import {BlurOnVisibilityDirective} from "./blur-on-visibility.directive";
 
 export const directives = [
+    BlurOnVisibilityDirective,
     UndraggableDirective,
     UnoptimizedImageDirective,
 ]
