90fe09f576539d57385daece0ac248fd236f70d0
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:48:32 -0500

    Add methods for bill deletion, categorize recurring bill intervals, and emit deletion events in bill.component.ts

--- angular/components/elements/bill/bill.component.ts
+++ angular/components/elements/bill/bill.component.ts
@@ -1,7 +1,14 @@
 // bill.component.ts 
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {OneTimeBill, RecurringBill} from "../../../models/paycheck/types";
 import {TagType} from "../../../models/html/TagType";
+import {BillEmitter} from "./models/types";
+import {
+    HttpDeleteOneTimeBillService
+} from "../../../services/server/http/http-delete-one-time-bill.service";
+import {
+    HttpDeleteRecurringBillService
+} from "../../../services/server/http/http-delete-recurring-bill.service";
 
 @Component({
     selector: 'bill',
@@ -10,8 +17,61 @@ import {TagType} from "../../../models/html/TagType";
 })
 export class BillComponent {
     @Input() bill: OneTimeBill | RecurringBill;
-    constructor() {
+    @Output() billDeleted: BillEmitter = new EventEmitter<OneTimeBill | RecurringBill>();
+    constructor(private deleteOneTimeBillService: HttpDeleteOneTimeBillService,
+                private deleteRecurringBillService: HttpDeleteRecurringBillService) {
         
+    }
+
+    deleteBill(): void {
+        this.deleteBillFromServer();
+        this.billDeleted.emit(this.bill);
+    }
+
+    deleteBillFromServer() {
+        if ('dueDate' in this.bill) {
+            this.deleteOneTimeBillService.deleteOneTimeBill(this.bill.id).subscribe(() => {
+                console.log('Bill deleted');
+            });
+        } else {
+            this.deleteRecurringBillService.deleteRecurringBill(this.bill.id).subscribe(() => {
+                console.log('Bill deleted');
+            });
+        }
+    }
+
+    public getRecurringString(): string {
+        if (this.bill.type === 'OneTimeBill') {
+            return 'One-Time';
+        } else {
+            return this.getIntervalString();
+        }
+    }
+
+    getIntervalString(): string {
+        if ('recurringDate' in this.bill) {
+            const interval: number = this.bill.recurringDate.interval;
+            switch (interval) {
+                case 1:
+                    return 'Yearly';
+                case 2:
+                    return 'Bi-Yearly';
+                case 4:
+                    return 'Quarterly';
+                case 6:
+                    return 'Bi-Monthly';
+                case 12:
+                    return 'Monthly';
+                case 26:
+                    return 'Bi-Weekly';
+                case 52:
+                    return 'Weekly';
+                case 365:
+                    return 'Daily';
+                default:
+                    throw new Error('Invalid interval value');
+            }
+        }
     }
 
     getAmountString(): string {
