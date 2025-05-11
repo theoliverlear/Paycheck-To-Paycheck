387a2584bfd6110e13682c665e8774559432c3fe
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:41:21 -0500

    Add types.ts to define types used in IncomeComponent

---
+++ angular/components/elements/income/models/types.ts
@@ -0,0 +1,4 @@
+import { EventEmitter } from '@angular/core';
+import { OneTimeIncome, RecurringIncome, WageIncome } from '../../../../models/paycheck/types';
+
+export type IncomeEmitter = EventEmitter<OneTimeIncome | RecurringIncome | WageIncome>;
