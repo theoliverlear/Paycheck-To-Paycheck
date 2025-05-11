85a1c7ba31221bc8e6b345e7b5c5ba829e714f80
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:23:22 -0500

    Add URL paths for deleting one-time and recurring bills in urls.py

--- backend/apps/views/bill/urls.py
+++ backend/apps/views/bill/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from backend.apps.views.bill.delete_one_time_bill_view import \
+    DeleteOneTimeBillView
+from backend.apps.views.bill.delete_recurring_bill_view import \
+    DeleteRecurringBillView
 from backend.apps.views.bill.get_all_bills_view import GetAllBillsView
 
 urlpatterns = [
     path('get/all', GetAllBillsView.as_view(), name="get_all_bills"),
+    path('delete/one-time/<int:bill_id>', DeleteOneTimeBillView.as_view(), name="delete_one_time_bill"),
+    path('delete/recurring/<int:bill_id>', DeleteRecurringBillView.as_view(), name="delete_recurring_bill"),
 ]
