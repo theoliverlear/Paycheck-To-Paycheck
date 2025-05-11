ca4de5d7c1a69962f43ab351db6118b271f648b7
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:38:53 -0500

    Add WageIncome deletion from User objects in wage_income_service.py

--- backend/apps/services/income/wage_income_service.py
+++ backend/apps/services/income/wage_income_service.py
@@ -1,7 +1,9 @@
+from channels.db import database_sync_to_async
 from injector import inject
 
 from backend.apps.entity.income.income_history import IncomeHistory
 from backend.apps.entity.income.wage_income import WageIncome
+from backend.apps.entity.user.user import User
 from backend.apps.repository.income.wage_income_repository import \
     WageIncomeRepository
 
@@ -16,3 +18,13 @@ class WageIncomeService:
 
     async def get_all_by_income_history(self, income_history: IncomeHistory) -> list[WageIncome]:
         return await self.wage_income_repository.get_all_by_income_history(income_history)
+
+    async def delete_from_user(self,
+                               user: User,
+                               wage_income_id: int) -> None:
+        user.user_income_history.wage_incomes = []
+        for income in user.user_income_history.wage_incomes:
+            if income.id != wage_income_id:
+                user.user_income_history.wage_incomes.append(income)
+        await user.user_income_history.save()
+        await database_sync_to_async(self.wage_income_repository.delete_by_id)(wage_income_id)
