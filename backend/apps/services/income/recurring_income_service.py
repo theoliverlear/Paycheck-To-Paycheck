03b814c0d17c3ac6c8318729decf17b6d531dd8a
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:37:37 -0500

    Add RecurringIncome deletion from User objects in recurring_income_service.py

--- backend/apps/services/income/recurring_income_service.py
+++ backend/apps/services/income/recurring_income_service.py
@@ -1,13 +1,16 @@
+from channels.db import database_sync_to_async
 from injector import inject
 
 from backend.apps.entity.income.recurring_income import RecurringIncome
+from backend.apps.entity.user.user import User
 from backend.apps.repository.income.recurring_income_repository import \
     RecurringIncomeRepository
 
 
 class RecurringIncomeService:
     @inject
-    def __init__(self, recurring_income_repository: RecurringIncomeRepository):
+    def __init__(self,
+                 recurring_income_repository: RecurringIncomeRepository):
         self.recurring_income_repository: RecurringIncomeRepository = recurring_income_repository
 
     def get_by_id(self, recurring_income_id: int) -> RecurringIncome:
@@ -15,3 +18,15 @@ class RecurringIncomeService:
 
     async def get_all_by_income_history(self, income_history) -> list[RecurringIncome]:
         return await self.recurring_income_repository.get_all_by_income_history(income_history)
+
+    async def delete_from_user(self,
+                               user: User,
+                               recurring_income_id: int) -> None:
+        user.user_income_history.recurring_incomes = []
+        for recurring_income in user.user_income_history.recurring_incomes:
+            if recurring_income.id != recurring_income_id:
+                user.user_income_history.recurring_incomes.append(
+                    recurring_income)
+        await user.user_income_history.save()
+        await database_sync_to_async(
+            self.recurring_income_repository.delete_by_id)(recurring_income_id)
