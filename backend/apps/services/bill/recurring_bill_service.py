528a2ff681d771e747228ea79477b24f91c89405
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:36:35 -0500

    Add RecurringBill deletion from User objects in recurring_bill_service.py

--- backend/apps/services/bill/recurring_bill_service.py
+++ backend/apps/services/bill/recurring_bill_service.py
@@ -1,6 +1,8 @@
+from channels.db import database_sync_to_async
 from injector import inject
 
 from backend.apps.entity.bill.recurring_bill import RecurringBill
+from backend.apps.entity.user.user import User
 from backend.apps.repository.bill.recurring_bill_repository import \
     RecurringBillRepository
 
@@ -16,3 +18,13 @@ class RecurringBillService:
 
     async def get_all_by_bill_history(self, bill_history) -> list[RecurringBill]:
         return await self.recurring_bill_repository.get_all_by_bill_history(bill_history)
+
+    async def delete_from_user(self,
+                               user: User,
+                               bill_id: int) -> None:
+        user.user_bill_history.recurring_bills = []
+        for bill in user.user_bill_history.recurring_bills:
+            if bill.id != bill_id:
+                user.user_bill_history.recurring_bills.append(bill)
+        await user.user_bill_history.save()
+        await database_sync_to_async(self.recurring_bill_repository.delete_by_id)(bill_id)
