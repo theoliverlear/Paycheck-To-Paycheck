7056a0226ad18f59a37c6704f184f9d1ca7a7c9e
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 12:21:09 -0500

    Add delete_wage_income_view.py to handle deletion of wage incomes through HTTP requests

---
+++ backend/apps/views/income/delete_wage_income_view.py
@@ -0,0 +1,51 @@
+from asgiref.sync import async_to_sync
+from injector import inject
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from backend.apps.comm.serialize.models.http.payload_status_response_serializer import \
+    PayloadStatusResponseSerializer
+from backend.apps.entity.user.user import User
+from backend.apps.models.dict.entity.income.wage_income_dict_parser import \
+    WageIncomeDictParser
+from backend.apps.models.http.communication_type import CommunicationType
+from backend.apps.models.http.operation_sucess_status import \
+    OperationSuccessStatus
+from backend.apps.services.auth_service import AuthService
+from backend.apps.services.income.wage_income_service import WageIncomeService
+from backend.apps.services.session.session_service import SessionService
+
+
+class DeleteWageIncomeView(APIView):
+    @inject
+    def __init__(self,
+                 auth_service: AuthService,
+                 session_service: SessionService,
+                 wage_income_service: WageIncomeService,
+                 wage_income_dict_parser: WageIncomeDictParser):
+        super().__init__()
+        self.auth_service: AuthService = auth_service
+        self.session_service: SessionService = session_service
+        self.wage_income_service: WageIncomeService = wage_income_service
+        self.wage_income_dict_parser: WageIncomeDictParser = wage_income_dict_parser
+
+    async def async_delete(self, http_request, income_id: int, *args, **kwargs):
+        user_in_session: bool = await self.auth_service.user_in_session(
+            communication_type=CommunicationType.HTTP,
+            http_request=http_request
+        )
+        if user_in_session:
+            user: User = await self.session_service.get_user_from_session(http_request)
+            await self.wage_income_service.delete_from_user(user, income_id)
+            serializer: PayloadStatusResponseSerializer = PayloadStatusResponseSerializer(
+                OperationSuccessStatus.OPERATION_SUCCESS.value
+            )
+            return Response(status=200, data=serializer.data)
+        else:
+            serializer: PayloadStatusResponseSerializer = PayloadStatusResponseSerializer(
+                OperationSuccessStatus.OPERATION_DENIED.value
+            )
+            return Response(status=401, data=serializer.data)
+
+    def delete(self, http_request, income_id, *args, **kwargs):
+        return async_to_sync(self.async_delete)(http_request, income_id, *args, **kwargs)
