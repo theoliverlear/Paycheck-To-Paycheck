060a29c0e513f2915d60124de9d509a5ed990ea2
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:37:04 -0500

    Add OneTimeIncome deletion from User objects in one_time_income_service.py

--- backend/apps/services/income/one_time_income_service.py
+++ backend/apps/services/income/one_time_income_service.py
@@ -1,6 +1,8 @@
+from channels.db import database_sync_to_async
 from injector import inject
 
 from backend.apps.entity.income.income_history import IncomeHistory
+from backend.apps.entity.user.user import User
 from backend.apps.repository.income.one_time_income_repository import \
     OneTimeIncomeRepository
 
@@ -15,3 +17,10 @@ class OneTimeIncomeService:
 
     async def get_all_by_income_history(self, income_history: IncomeHistory):
         return await self.income_repository.get_all_by_income_history(income_history)
+
+    async def delete_from_user(self,
+                               user: User,
+                               income_id: int) -> None:
+        user.user_income_history.one_time_incomes = [income for income in user.user_income_history.one_time_incomes if income.id != income_id]
+        await user.user_income_history.save()
+        await database_sync_to_async(self.income_repository.delete_by_id)(income_id)
