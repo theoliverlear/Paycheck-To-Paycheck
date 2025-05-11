9828484edf6f1ec8d05304ec5f6af39d097d73f8
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 12:20:18 -0500

    Add delete income endpoints to urls.py for one-time, recurring, and wage incomes in the backend

--- backend/apps/views/income/urls.py
+++ backend/apps/views/income/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
+from backend.apps.views.income.delete_one_time_income_view import \
+    DeleteOneTimeIncomeView
+from backend.apps.views.income.delete_recurring_income_view import \
+    DeleteRecurringIncomeView
+from backend.apps.views.income.delete_wage_income_view import \
+    DeleteWageIncomeView
 from backend.apps.views.income.get_all_incomes_view import GetAllIncomesView
 
 urlpatterns = [
     path('get/all', GetAllIncomesView.as_view(), name="get_all_incomes"),
+    path('delete/one-time/<int:income_id>', DeleteOneTimeIncomeView.as_view(), name="delete_one_time_income"),
+    path('delete/recurring/<int:income_id>', DeleteRecurringIncomeView.as_view(), name="delete_recurring_income"),
+    path('delete/wage/<int:income_id>', DeleteWageIncomeView.as_view(), name="delete_wage_income"),
 ]
