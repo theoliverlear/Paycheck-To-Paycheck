37eebe7008539b812a398b9fb6d62689c2892af5
theoliverlear <sigwartholiver2@gmail.com>
2025-05-11 15:36:08 -0500

    Add OneTimeBill deletion from User objects in one_time_bill_service.py

--- backend/apps/services/bill/one_time_bill_service.py
+++ backend/apps/services/bill/one_time_bill_service.py
@@ -1,6 +1,8 @@
+from channels.db import database_sync_to_async
 from injector import inject
 
 from backend.apps.entity.bill.one_time_bill import OneTimeBill
+from backend.apps.entity.user.user import User
 from backend.apps.repository.bill.one_time_bill_repository import \
     OneTimeBillRepository
 
@@ -16,3 +18,10 @@ class OneTimeBillService:
 
     async def get_all_by_bill_history(self, bill_history) -> list[OneTimeBill]:
         return await self.one_time_bill_repository.get_all_by_bill_history(bill_history)
+
+    async def delete_from_user(self,
+                               user: User,
+                               bill_id: int) -> None:
+        user.user_bill_history.one_time_bills = [bill for bill in user.user_bill_history.one_time_bills if bill.id != bill_id]
+        await user.user_bill_history.save()
+        await database_sync_to_async(self.one_time_bill_repository.delete_by_id)(bill_id)
