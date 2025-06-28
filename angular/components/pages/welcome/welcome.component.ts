fb2d3bd35ebc808fdce1ad8be334fa74522a55d2
theoliverlear <sigwartholiver2@gmail.com>
2025-06-28 18:44:31 -0500

    Expand welcome.component.ts to include recent paycheck submission functionality

--- angular/components/pages/welcome/welcome.component.ts
+++ angular/components/pages/welcome/welcome.component.ts
@@ -1,5 +1,11 @@
 // welcome.component.ts 
 import { Component } from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+import {WelcomeService} from "../../../services/server/welcome.service";
+import {
+    LastPaycheck
+} from "../../elements/element-group-welcome/last-paycheck-input/models/LastPaycheck";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'welcome',
@@ -7,7 +13,22 @@ import { Component } from "@angular/core";
     styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
-    constructor() {
+    constructor(private welcomeService: WelcomeService,
+                private router: Router) {
         
     }
+
+
+    submitWelcomeSurvey(lastPaycheck: LastPaycheck): void {
+        console.log('Clicked submit welcome survey with:', lastPaycheck);
+        this.welcomeService.submitWelcomeSurvey(lastPaycheck).subscribe(operationSuccessResponse => {
+            console.log('Welcome survey submitted successfully:', operationSuccessResponse);
+            if (operationSuccessResponse.operationSuccess) {
+                this.router.navigate(['/budget']);
+                console.log('Redirecting to budget page');
+            }
+        });
+    }
+
+    protected readonly TagType = TagType;
 }
