250b283467fdb6287455b75e730ced8f00344bc6
theoliverlear <sigwartholiver2@gmail.com>
2025-06-28 19:18:49 -0500

    Add routing for welcome views in urls.py

--- backend/apps/views/urls.py
+++ backend/apps/views/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('income/', include('backend.apps.views.income.urls')),
     path('paycheck/', include('backend.apps.views.paycheck.urls')),
     path('user/', include('backend.apps.views.user.urls')),
-    path('wallet/', include('backend.apps.views.wallet.urls'))
+    path('wallet/', include('backend.apps.views.wallet.urls')),
+    path('welcome/', include('backend.apps.views.welcome.urls'))
 ]
