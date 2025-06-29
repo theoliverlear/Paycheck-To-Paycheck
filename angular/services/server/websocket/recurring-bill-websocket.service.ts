a6ae40333097076de021ac9b13fbf9b8ba25d65c
theoliverlear <sigwartholiver2@gmail.com>
2025-06-29 17:41:42 -0500

    Edit recurring-bill-websocket.service.ts to type the WebSocket response type

--- angular/services/server/websocket/recurring-bill-websocket.service.ts
+++ angular/services/server/websocket/recurring-bill-websocket.service.ts
@@ -2,11 +2,14 @@ import {Injectable} from "@angular/core";
 import {WebSocketService} from "./websocket.service";
 import {Bill} from "../../../models/bill/Bill";
 import {environment} from "../../../environments/environment";
+import {
+    WebSocketRecurringBill
+} from "../../../models/paycheck/types";
 
 @Injectable({
     providedIn: 'root'
 })
-export class RecurringBillWebSocketService extends WebSocketService<Bill, any> {
+export class RecurringBillWebSocketService extends WebSocketService<Bill, WebSocketRecurringBill> {
     private static readonly URL: string = `${environment.webSocketUrl}/recurring-bill`;
     constructor() {
         super(RecurringBillWebSocketService.URL);
