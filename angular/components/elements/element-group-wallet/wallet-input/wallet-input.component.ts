898f8f5376d48ac6d102151b420c9e7ea259722e
theoliverlear <sigwartholiver2@gmail.com>
2025-06-29 17:25:58 -0500

    Add wallet-input.component.ts to be an Angular component for the wallet-input element in the Angular application

---
+++ angular/components/elements/element-group-wallet/wallet-input/wallet-input.component.ts
@@ -0,0 +1,13 @@
+// wallet-input.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'wallet-input',
+    templateUrl: './wallet-input.component.html',
+    styleUrls: ['./wallet-input.component.css']
+})
+export class WalletInputComponent {
+    constructor() {
+        
+    }
+}
