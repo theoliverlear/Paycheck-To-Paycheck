a2afc781cb40bd5c110d800bf4559be427b73aa7
theoliverlear <sigwartholiver2@gmail.com>
2025-06-29 17:41:19 -0500

    Edit one-time-bill-websocket.service.ts to type the WebSocket response type

--- angular/services/server/websocket/one-time-bill-websocket.service.ts
+++ angular/services/server/websocket/one-time-bill-websocket.service.ts
@@ -2,11 +2,12 @@ import {Injectable} from "@angular/core";
 import {WebSocketService} from "./websocket.service";
 import {Bill} from "../../../models/bill/Bill";
 import {environment} from "../../../environments/environment";
+import {WebSocketOneTimeBill} from "../../../models/paycheck/types";
 
 @Injectable({
     providedIn: 'root'
 })
-export class OneTimeBillWebsocketService extends WebSocketService<Bill, any> {
+export class OneTimeBillWebsocketService extends WebSocketService<Bill, WebSocketOneTimeBill> {
     private static readonly URL: string = `${environment.webSocketUrl}/one-time-bill`;
     constructor() {
         super(OneTimeBillWebsocketService.URL);
